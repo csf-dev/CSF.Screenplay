204483ce8a90a36120c0c556d7a764435fe5e5ea
Craig Fowler <craig@csf-dev.com>
2017-07-28 22:06:01 +0100

    Bump all version numbers for v0.1.0 release

--- CSF.Screenplay.Web/Properties/AssemblyInfo.cs
+++ CSF.Screenplay.Web/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes. 
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("CSF.Screenplay.Selenium")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CSF.Screenplay.Web")]
+[assembly: AssemblyDescription("Selenium WebDriver 3.x bindings for Screenplay")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("CSF Software Limited")]
 [assembly: AssemblyProduct("")]
@@ -17,7 +17,7 @@ using System.Runtime.CompilerServices;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0")]
 
 // The following attributes are used to specify the signing key for the assembly, 
 // if desired. See the Mono documentation for more information about signing.
