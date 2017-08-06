597c5e4ba6f31343d5dbecd2ba1dad9cd28c8a7b
Craig Fowler <craig@csf-dev.com>
2017-08-06 14:47:45 +0100

    Trivial improvement to AppVeyor scripts (use of echo)

--- .appveyor.after_deploy.bat
+++ .appveyor.after_deploy.bat
@@ -1,7 +1,12 @@
 @echo on
 
 nunit3-console.exe Tests\CSF.Screenplay.Web.Tests\bin\Debug\CSF.Screenplay.Web.Tests.dll
-    
+
+@echo.
+@echo *****************
 @echo Screenplay report
 @echo *****************
-type screenplay-report.txt
+
+@type screenplay-report.txt
+
+@echo off
--- .appveyor.before_build.bat
+++ .appveyor.before_build.bat
@@ -1,6 +1,9 @@
 @echo on
+
 git submodule update --init --recursive
 
 nuget restore CSF.Screenplay.sln
 
 copy /y Tests\CSF.Screenplay.Web.Tests\App.config.AppVeyor Tests\CSF.Screenplay.Web.Tests\App.config
+
+@echo off
