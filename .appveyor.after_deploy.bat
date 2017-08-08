a40fc65cfdbd39a20aa7072eda8dfe69cc759702
Craig Fowler <craig@csf-dev.com>
2017-08-08 13:02:36 +0100

    WIP #45 - Attempt to fix AppVeyor build script (exit codes)

--- .appveyor.after_deploy.bat
+++ .appveyor.after_deploy.bat
@@ -1,8 +1,15 @@
-@echo on
+﻿@echo on
+
+@SET /A exitcode=0
+@SET /A TESTFAILURE_ERROR=1
+@SET /A PUSHARTIFACT_ERROR=2
+@SET /A READREPORT_ERROR=4
 
 nunit3-console.exe Tests\CSF.Screenplay.Web.Tests\bin\Debug\CSF.Screenplay.Web.Tests.dll
+@IF %ERRORLEVEL% NEQ 0 SET /A exitcode^|=%TESTFAILURE_ERROR%
 
 appveyor PushArtifact screenplay-report.txt
+@IF %ERRORLEVEL% NEQ 0 SET /A exitcode^|=%PUSHARTIFACT_ERROR%
 
 @echo.
 @echo *****************
@@ -10,5 +17,8 @@ appveyor PushArtifact screenplay-report.txt
 @echo *****************
 
 @type screenplay-report.txt
+@IF %ERRORLEVEL% NEQ 0 SET /A exitcode^|=%READREPORT_ERROR%
 
 @echo off
+
+@EXIT /B %exitcode%
