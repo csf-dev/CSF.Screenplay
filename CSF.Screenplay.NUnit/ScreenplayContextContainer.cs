b6ed5100340e0dcda094069921c66a2b0e2ef374
Craig Fowler <craig@csf-dev.com>
2017-08-09 19:52:06 +0100

    WIP #44 - Add way of injecting context into NUnit integration

---
+++ CSF.Screenplay.NUnit/ScreenplayContextContainer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace CSF.Screenplay.NUnit
+{
+  static class ScreenplayContextContainer
+  {
+    static readonly IDictionary<Assembly,ScreenplayContext> contexts;
+    static object syncRoot;
+
+    internal static ScreenplayContext GetContext(Type fixtureType)
+    {
+      if(fixtureType == null)
+        throw new ArgumentNullException(nameof(fixtureType));
+      
+      return GetContext(fixtureType.Assembly);
+    }
+
+    internal static ScreenplayContext GetContext(Assembly assembly)
+    {
+      if(assembly == null)
+        throw new ArgumentNullException(nameof(assembly));
+
+      lock(syncRoot)
+      {
+        if(!contexts.ContainsKey(assembly))
+        {
+          var context = new ScreenplayContext();
+          contexts.Add(assembly, context);
+        }
+      }
+
+      return contexts[assembly];
+    }
+
+    static ScreenplayContextContainer()
+    {
+      contexts = new Dictionary<Assembly,ScreenplayContext>();
+      syncRoot = new object();
+    }
+  }
+}
---
+++ CSF.Screenplay.NUnit/ScreenplayFixtureAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
+
+namespace CSF.Screenplay.NUnit
+{
+  public class ScreenplayFixtureAttribute : TestFixtureAttribute
+  {
+    readonly NUnitTestFixtureBuilder builder;
+
+    public new IEnumerable<TestSuite> BuildFrom (ITypeInfo typeInfo)
+    {
+      var data = new TestFixtureData(ScreenplayContextContainer.GetContext(typeInfo.Assembly));
+      var output = builder.BuildFrom(typeInfo, data);
+      return new [] { output };
+    }
+
+    public ScreenplayFixtureAttribute() : this(new object[0]) {}
+
+    public ScreenplayFixtureAttribute(params object[] arguments) : base(arguments)
+    {
+      builder = new NUnitTestFixtureBuilder();
+    }
+  }
+}
