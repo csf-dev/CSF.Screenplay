d7e4a4710b0ac73bf5629de7bc11f6d3adfe689f
Craig Fowler <craig@csf-dev.com>
2017-08-16 23:46:25 +0100

    WIP #25 - Improve reporting of current status in build script

--- .travis.build.sh
+++ .travis.build.sh
@@ -15,41 +15,48 @@ test_outcome=1
 stop_if_failure()
 {
   code="$1"
+  process="$2"
   if [ "$code" -ne "0" ]
   then
+    echo "The process '${process}' failed with exit code $code"
     exit "$code"
   fi
 }
 
 build_solution()
 {
+  echo "Building the solution ..."
   msbuild /p:Configuration=Debug CSF.Screenplay.sln
-  stop_if_failure $?
+  stop_if_failure $? "Build the solution"
 }
 
 run_unit_tests()
 {
+  echo "Running unit tests ..."
   test_assemblies=$(find ./Tests/ -type f -path "*/bin/Debug/*" -name "$TEST_PATTERN" \! -name "$WEB_TESTS")
   mono "$NUNIT_PATH" $test_assemblies
-  stop_if_failure $?
+  stop_if_failure $? "Run unit tests"
 }
 
 start_webserver()
 {
+  echo "Starting the app on a web server ..."
   xsp4 --nonstop --address "$SERVER_ADDR" --port "$SERVER_PORT" --applications "$SERVER_WEB_APP" --pidfile "$SERVER_PID" &
 }
 
 wait_for_app_to_become_available()
 {
+  echo "Waiting for the app to become available ..."
   sleep 1
   wget -O - "$APP_HOMEPAGE" >/dev/null 2>&1
-  stop_if_failure $?
+  stop_if_failure $? "Wait for the app to start up"
 }
 
 run_integration_tests()
 {
+  echo "Running integration tests ..."
   mono "$NUNIT_PATH" "$WEB_TESTS_PATH"
-  test_outcome=$?
+  test_outcome=$? "Run integration tests"
 }
 
 shutdown_webserver()
--- .travis.install.sh
+++ .travis.install.sh
@@ -8,18 +8,22 @@ NUGET_PATH=".nuget/nuget.exe"
 stop_if_failure()
 {
   code="$1"
+  process="$2"
   if [ "$code" -ne "0" ]
   then
+    echo "The process '${process}' failed with exit code $code"
     exit "$code"
   fi
 }
 
 install_latest_nuget()
 {
+  echo "Downloading the latest version of NuGet ..."
+
   # Travis uses Xamarin's apt repo which has an ancient nuget version
   mkdir -p .nuget
   wget -O "$NUGET_PATH" "$NUGET_LATEST_DIST"
-  stop_if_failure $?
+  stop_if_failure $? "Download NuGet"
 }
 
 echo_nuget_version_to_console()
@@ -29,20 +33,23 @@ echo_nuget_version_to_console()
 
 setup_travis_test_config()
 {
+  echo "Copying Travis-specific test configs ..."
   cp "$TRAVIS_TEST_CONFIG_SOURCE" "$TEST_CONFIG_DESTINATION"
-  stop_if_failure $?
+  stop_if_failure $? "Setup Travis configuration"
 }
 
 install_test_runner()
 {
+  echo "Downloading an NUnit test runner ..."
   mono "$NUGET_PATH" install NUnit.ConsoleRunner -Version 3.6.1 -OutputDirectory testrunner
-  stop_if_failure $?
+  stop_if_failure $? "Download NUnit test runner"
 }
 
 restore_solution_nuget_packages()
 {
+  echo "Restoring NuGet packages for the solution ..."
   mono "$NUGET_PATH" restore CSF.Screenplay.sln
-  stop_if_failure $?
+  stop_if_failure $? "Restore NuGet packages"
 }
 
 install_latest_nuget
