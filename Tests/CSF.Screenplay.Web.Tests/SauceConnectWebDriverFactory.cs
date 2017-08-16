2ad50a331e3a33ec42e07da915a01fd739d3354c
Craig Fowler <craig@csf-dev.com>
2017-08-16 22:13:00 +0100

    WIP #25 - Attempt to fix Sauce Labs auth

--- Tests/CSF.Screenplay.Web.Tests/SauceConnectWebDriverFactory.cs
+++ Tests/CSF.Screenplay.Web.Tests/SauceConnectWebDriverFactory.cs
@@ -7,12 +7,23 @@ namespace CSF.Screenplay.Web.Tests
   {
     const string
       TunnelIdCapability = "tunnel-identifier",
-      TunnelIdEnvVariable = "TRAVIS_JOB_NUMBER";
+      TunnelIdEnvVariable = "TRAVIS_JOB_NUMBER",
+      SauceUsernameEnvVariable = "SAUCE_USERNAME",
+      SauceApiKeyEnvVariable = "SAUCE_ACCESS_KEY",
+      SauceLabsEndpointFormat = "http://{0}:{1}ondemand.saucelabs.com/wd/hub";
 
     protected override void ConfigureCapabilities(OpenQA.Selenium.Remote.DesiredCapabilities caps)
     {
       base.ConfigureCapabilities(caps);
       caps.SetCapability(TunnelIdCapability, Environment.GetEnvironmentVariable(TunnelIdEnvVariable));
+    }
+
+    protected override Uri GetRemoteUri()
+    {
+      var username = Environment.GetEnvironmentVariable(SauceUsernameEnvVariable);
+      var apiKey = Environment.GetEnvironmentVariable(SauceApiKeyEnvVariable);
+      var endpoint = String.Format(SauceLabsEndpointFormat, username, apiKey);
+      return new Uri(endpoint);
     }
   }
 }
