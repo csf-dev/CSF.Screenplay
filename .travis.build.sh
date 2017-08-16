eced2908f03066a892aaf2f5f6f9baafc1780136
Craig Fowler <craig@csf-dev.com>
2017-08-16 22:53:18 +0100

    WIP #25 - Ensure results are printed to console, and re-enable build matrix

--- .travis.build.sh
+++ .travis.build.sh
@@ -8,6 +8,7 @@ SERVER_PORT="8080"
 SERVER_ADDR="127.0.0.1"
 SERVER_WEB_APP="/:Tests/CSF.Screenplay.WebTestWebsite/"
 SERVER_PID=".xsp4.pid"
+WEBSERVER_PAUSE_SECONDS="2"
 
 test_outcome=1
 
@@ -36,8 +37,8 @@ run_unit_tests()
 start_webserver()
 {
   xsp4 --nonstop --address "$SERVER_ADDR" --port "$SERVER_PORT" --applications "$SERVER_WEB_APP" --pidfile "$SERVER_PID" &
-  echo "Waiting for 10 seconds for the web server to come up ..."
-  sleep 10
+  echo "Waiting for $WEBSERVER_PAUSE_SECONDS seconds for the web server to come up ..."
+  sleep $WEBSERVER_PAUSE_SECONDS
 }
 
 run_integration_tests()
@@ -53,10 +54,16 @@ shutdown_webserver()
   rm "$SERVER_PID"
 }
 
+echo_integration_test_results_to_console()
+{
+  cat SpecFlow.report.txt
+}
+
 build_solution
 run_unit_tests
 start_webserver
 run_integration_tests
+echo_integration_test_results_to_console
 shutdown_webserver
 
 exit $test_outcome
