127f89525dffed5781cda6780b8fc04ce8a2a2e3
Craig Fowler <craig@csf-dev.com>
2017-08-16 20:37:21 +0100

    WIP #25 - Add Travis-specific config

---
+++ Tests/CSF.Screenplay.Web.Tests/SauceConnectWebDriverFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using CSF.WebDriverFactory.Impl;
+
+namespace CSF.Screenplay.Web.Tests
+{
+  public class SauceConnectWebDriverFactory : RemoteWebDriverFromEnvironmentFactory
+  {
+    const string
+      TunnelIdCapability = "tunnel-identifier",
+      TunnelIdEnvVariable = "TRAVIS_JOB_NUMBER";
+
+    protected override void ConfigureCapabilities(OpenQA.Selenium.Remote.DesiredCapabilities caps)
+    {
+      base.ConfigureCapabilities(caps);
+      caps.SetCapability(TunnelIdCapability, Environment.GetEnvironmentVariable(TunnelIdEnvVariable));
+    }
+  }
+}
