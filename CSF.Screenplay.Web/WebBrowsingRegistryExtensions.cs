02c84bfa54873a2462350ffd17bba40551c4245d
Craig Fowler <craig@csf-dev.com>
2017-08-19 20:27:38 +0100

    WIP #52 - Rework web components

---
+++ CSF.Screenplay.Web/WebBrowsingRegistryExtensions.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using CSF.Screenplay.Scenarios;
+using CSF.Screenplay.Web.Abilities;
+using OpenQA.Selenium;
+
+namespace CSF.Screenplay
+{
+  public static class WebBrowsingRegistryExtensions
+  {
+    public static void RegisterWebDriver(this IServiceRegistryBuilder builder,
+                                         Func<IServiceResolver,IWebDriver> factory,
+                                         string name = null)
+    {
+      if(builder == null)
+        throw new ArgumentNullException(nameof(builder));
+      if(factory == null)
+        throw new ArgumentNullException(nameof(factory));
+
+      builder.RegisterPerScenario(factory, name);
+    }
+
+    public static void RegisterWebDriver(this IServiceRegistryBuilder builder,
+                                         IWebDriver driver,
+                                         string name = null)
+    {
+      if(builder == null)
+        throw new ArgumentNullException(nameof(builder));
+      if(driver == null)
+        throw new ArgumentNullException(nameof(driver));
+
+      builder.RegisterSingleton(driver, name);
+    }
+
+    public static void RegisterUriTransformer(this IServiceRegistryBuilder builder,
+                                              Func<IServiceResolver,IUriTransformer> factory = null,
+                                              string name = null)
+    {
+      if(builder == null)
+        throw new ArgumentNullException(nameof(builder));
+
+      if(factory != null)
+        builder.RegisterPerScenario(factory, name);
+
+      builder.RegisterPerScenario(res => NoOpUriTransformer.Default, name);
+    }
+
+    public static void RegisterUriTransformer(this IServiceRegistryBuilder builder,
+                                              IUriTransformer transformer,
+                                              string name = null)
+    {
+      if(builder == null)
+        throw new ArgumentNullException(nameof(builder));
+      if(transformer == null)
+        throw new ArgumentNullException(nameof(transformer));
+
+      builder.RegisterSingleton(transformer, name);
+    }
+
+    public static void RegisterWebBrowser(this IServiceRegistryBuilder builder,
+                                          Func<IServiceResolver,BrowseTheWeb> factory = null,
+                                          string name = null)
+    {
+      if(builder == null)
+        throw new ArgumentNullException(nameof(builder));
+
+      if(factory != null)
+        builder.RegisterPerScenario(factory, name);
+
+      builder.RegisterPerScenario(CreateWebBrowser, name);
+    }
+
+    static BrowseTheWeb CreateWebBrowser(IServiceResolver resolver)
+    {
+      if(resolver == null)
+        throw new ArgumentNullException(nameof(resolver));
+
+      var driver = resolver.GetService<IWebDriver>();
+      var transformer = resolver.GetOptionalService<IUriTransformer>();
+      return new BrowseTheWeb(driver, transformer?? NoOpUriTransformer.Default);
+    }
+  }
+}
---
+++ CSF.Screenplay.Web/WebBrowsingResolverExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using CSF.Screenplay.Scenarios;
+using CSF.Screenplay.Web.Abilities;
+
+namespace CSF.Screenplay
+{
+  public static class WebBrowsingResolverExtensions
+  {
+    public static BrowseTheWeb GetWebBrowser(this IServiceResolver resolver, string name = null)
+    {
+      if(resolver == null)
+        throw new ArgumentNullException(nameof(resolver));
+
+      return resolver.GetService<BrowseTheWeb>(name);
+    }
+  }
+}
