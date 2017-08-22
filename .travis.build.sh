7f1b877b4e3ad21a9014893ca24bec49e38e1461
Craig Fowler <craig@csf-dev.com>
2017-08-22 21:44:52 +0100

    Looks like travis_wait has to go in the YML

--- .travis.build.sh
+++ .travis.build.sh
@@ -45,7 +45,7 @@ start_webserver()
 run_integration_tests()
 {
   echo "Running integration tests ..."
-  travis_wait 30 mono "$NUNIT_PATH" --remoting-wait-after-stop --remoting-shutdown-delay=5000 "$WEB_TESTS_PATH"
+  mono "$NUNIT_PATH" --remoting-wait-after-stop --remoting-shutdown-delay=5000 "$WEB_TESTS_PATH"
   test_outcome=$?
 }
 
