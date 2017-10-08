5e58221104652903140146401754cae95d57a42c
Craig Fowler <craig@csf-dev.com>
2017-10-08 18:40:35 +0100

    Resolve #89 - Add Sauce Labs webdriver factory

---
+++ CSF.WebDriverFactory/Impl/SauceConnectWebDriverFactory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace CSF.WebDriverFactory.Impl
+{
+  public abstract class SauceConnectWebDriverFactory : RemoteWebDriverFromEnvironmentFactory
+  {
+    const string
+      TunnelIdCapability = "tunnel-identifier",
+      UsernameCapability = "username",
+      ApiKeyCapability = "accessKey",
+      TestNameCapabilityName = "name",
+      BuildNameCapability = "build";
+
+    public static string TestNameCapability => TestNameCapabilityName;
+
+    protected override void ConfigureCapabilities(OpenQA.Selenium.Remote.DesiredCapabilities caps)
+    {
+      base.ConfigureCapabilities(caps);
+
+      caps.SetCapability(TunnelIdCapability, GetTunnelId());
+      caps.SetCapability(UsernameCapability, GetSauceUsername());
+      caps.SetCapability(ApiKeyCapability, GetSauceAccessKey());
+      caps.SetCapability(BuildNameCapability, GetSauceBuildName());
+    }
+
+    protected abstract string GetTunnelId();
+
+    protected abstract string GetSauceUsername();
+
+    protected abstract string GetSauceAccessKey();
+
+    protected abstract string GetSauceBuildName();
+  }
+}
