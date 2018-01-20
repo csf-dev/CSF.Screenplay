14455676eca2e7c2a0729546473272ba39f955f4
Craig Fowler <craig@csf-dev.com>
2018-01-20 16:48:26 +0000

    WIP #37 - Create basic framework for JSON APIs

---
+++ CSF.Screenplay.JsonApis/Abilities/ConsumeJsonWebServices.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using CSF.Screenplay.Abilities;
+using Newtonsoft.Json;
+
+namespace CSF.Screenplay.JsonApis.Abilities
+{
+  public class ConsumeJsonWebServices : Ability
+  {
+    #region fields
+
+    static readonly TimeSpan SystemDefaultTimeout = new TimeSpan(0, 0, 30);
+
+    readonly TimeSpan defaultTimeout;
+    readonly HttpClient httpClient;
+    readonly JsonSerializer serializer;
+
+    #endregion
+
+    #region public API
+
+    public virtual void Execute(IProvidesInvocationDetails invocationDetails)
+    {
+      var task = GetResponse(invocationDetails);
+
+      var timeout = GetTimeout(invocationDetails);
+      if(!task.Wait(timeout))
+        throw new TimeoutException($"Timeout waiting for a response from a JSON API.  Waited for {timeout.ToString("g")}");
+    }
+
+    public virtual T GetResult<T>(IProvidesInvocationDetails invocationDetails)
+    {
+      var response = GetResponse(invocationDetails);
+      var task = ConvertResponse<T>(response);
+
+      var timeout = GetTimeout(invocationDetails);
+      if(!task.Wait(timeout))
+        throw new TimeoutException($"Timeout waiting for a response from a JSON API.  Waited for {timeout.ToString("g")}");
+
+      return task.Result;
+    }
+
+    #endregion
+
+    #region private methods
+
+    async Task<HttpResponseMessage> GetResponse(IProvidesInvocationDetails invocationDetails)
+    {
+      if(invocationDetails == null)
+        throw new ArgumentNullException(nameof(invocationDetails));
+
+      var requestMessage = invocationDetails.GetRequestMessage();
+      var timeout = GetTimeout(invocationDetails);
+
+      var response = await httpClient.SendAsync(requestMessage);
+
+      AssertThatResultIsSuccess(response, timeout);
+
+      return response;
+    }
+
+    void AssertThatResultIsSuccess(HttpResponseMessage message, TimeSpan timeout)
+    {
+      try
+      {
+        message.EnsureSuccessStatusCode();
+      }
+      catch(HttpRequestException ex)
+      {
+        var content = message.Content.ReadAsStringAsync();
+        content.Wait(timeout);
+        var response = content.Result;
+
+        throw new JsonApiException($@"The API request failed
+{response}", ex);
+      }
+    }
+
+    protected virtual async Task<T> ConvertResponse<T>(Task<HttpResponseMessage> response)
+    {
+      var buffer = new MemoryStream();
+      await response.Result.Content.CopyToAsync(buffer);
+
+      using(var reader = new StreamReader(buffer))
+      {
+        return (T) serializer.Deserialize(reader, typeof(T));
+      }
+    }
+
+    TimeSpan GetTimeout(IProvidesInvocationDetails invocationDetails)
+      => invocationDetails.GetTimeout().GetValueOrDefault(defaultTimeout);
+
+    #endregion
+
+    #region boilerplate Ability overrides
+
+    protected override string GetReport(Actors.INamed actor)
+    => $"{actor.Name} can consume JSON web services.";
+
+    protected override void Dispose(bool disposing)
+    {
+      base.Dispose(disposing);
+
+      if(disposing)
+        httpClient.Dispose();
+    }
+
+    #endregion
+
+    #region constructor
+
+    public ConsumeJsonWebServices(Uri baseUri = null, TimeSpan? defaultTimeout = null)
+    {
+      httpClient = new HttpClient { BaseAddress = baseUri };
+      this.defaultTimeout = defaultTimeout.GetValueOrDefault(SystemDefaultTimeout);
+      serializer = JsonSerializer.CreateDefault();
+    }
+
+    #endregion
+  }
+}
---
+++ CSF.Screenplay.JsonApis/JsonServiceDescription.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace CSF.Screenplay.JsonApis
+{
+  public abstract class JsonServiceDescription : IProvidesInvocationDetails
+  {
+    static readonly ContentType JsonContentType = new ContentType("application/json");
+
+    readonly TimeSpan? timeout;
+    readonly object requestPayload;
+    readonly JsonSerializer serializer;
+
+    public HttpRequestMessage GetRequestMessage()
+    {
+      var output = new HttpRequestMessage
+      {
+        RequestUri = GetUri(),
+        Method = GetHttpMethod(),
+        Content = GetContent(),
+      };
+
+      ConfigureRequestHeaders(output);
+
+      return output;
+    }
+
+    public TimeSpan? GetTimeout() => timeout;
+
+    protected abstract Uri GetUri();
+
+    protected virtual HttpMethod GetHttpMethod() => HttpMethod.Post;
+
+    protected virtual IDictionary<string,string> GetHeaders() => null;
+
+    protected virtual Encoding GetEncoding() => Encoding.UTF8;
+
+    void ConfigureRequestHeaders(HttpRequestMessage request)
+    {
+      var headers = GetHeaders();
+      if(headers == null)
+        return;
+      
+      foreach(var header in headers.Keys)
+      {
+        request.Headers.Add(header, headers[header]);
+      }
+    }
+
+    HttpContent GetContent()
+    {
+      if(ReferenceEquals(requestPayload, null))
+        return null;
+
+      var serializedContent = SerializeContentToJson(requestPayload);
+      return new StringContent(serializedContent, GetEncoding(), JsonContentType.MediaType);
+    }
+
+    string SerializeContentToJson(object content)
+    {
+      var sb = new StringBuilder();
+
+      using(var writer = new StringWriter(sb))
+      {
+        serializer.Serialize(writer, content);
+        writer.Flush();
+      }
+
+      return sb.ToString();
+    }
+
+    public override string ToString() => $"the {GetType().Name} service";
+
+    public JsonServiceDescription(TimeSpan? timeout = null,
+                                  object requestPayload = null)
+    {
+      this.requestPayload = requestPayload;
+      this.timeout = timeout;
+
+      serializer = JsonSerializer.CreateDefault();
+    }
+  }
+}
