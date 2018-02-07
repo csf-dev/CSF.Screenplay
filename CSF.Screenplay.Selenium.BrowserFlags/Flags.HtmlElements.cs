08ab8bf3f2cd45a2b3d0af4a8917a566c71fac52
Craig Fowler <craig@csf-dev.com>
2018-02-07 22:43:33 +0000

    WIP #12 - Add the flags which are missing and needed for new ignore mechanism

--- CSF.Screenplay.Selenium.BrowserFlags/Flags.HtmlElements.cs
+++ CSF.Screenplay.Selenium.BrowserFlags/Flags.HtmlElements.cs
@@ -24,6 +24,26 @@ namespace CSF.Screenplay.Selenium
         /// </summary>
         public static readonly string CanEnterUsingIsoFormat = "HtmlElements.InputTypeDate.CanEnterUsingIsoFormat";
       }
+
+      /// <summary>
+      /// Flags relating to HTML <c>&lt;select multiple&gt;</c> elements.
+      /// </summary>
+      public static class SelectMultiple
+      {
+        /// <summary>
+        /// Indicates that the web driver must send <c>Ctrl+Click</c> in order to toggle the selection of a single
+        /// option within the select element.  Without Ctrl, the click is interpreted as "change entire selection to just
+        /// the one option clicked".
+        /// </summary>
+        public static readonly string RequiresCtrlClickToToggleOptionSelection = "HtmlElements.SelectMultiple.RequiresCtrlClickToToggleOptionSelection";
+
+        /// <summary>
+        /// Indicates that the web driver must send <c>Command+Click</c> (Command key = Mac) in order to
+        /// toggle the selection of a single option within the select element.  Without Command, the click
+        /// is interpreted as "change entire selection to just the one option clicked".
+        /// </summary>
+        public static readonly string RequiresCommandClickToToggleOptionSelection = "HtmlElements.SelectMultiple.RequiresCommandClickToToggleOptionSelection";
+      }
     }
   }
 }
