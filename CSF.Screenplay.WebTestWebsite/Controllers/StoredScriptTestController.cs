7cb313cc783ff7ce9ccb3e4e15bb4e7c381c150a
Craig Fowler <craig@csf-dev.com>
2018-03-06 21:20:27 +0000

    WIP #36 - Add a test harness for Jasmine-testing JavaScript resources

---
+++ CSF.Screenplay.WebTestWebsite/Controllers/StoredScriptTestController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CSF.Screenplay.Selenium.StoredScripts;
+using CSF.Screenplay.WebTestWebsite.Models;
+
+namespace CSF.Screenplay.WebTestWebsite.Controllers
+{
+  public class StoredScriptTestController : Controller
+  {
+    public ActionResult Index(string id)
+    {
+      var provider = GetScriptProvider(id);
+      if(provider == null) return HttpNotFound();
+
+      var model = new JavaScriptTestModel {
+        ScriptProvider = provider,
+        BaseUri = new Uri($"http://localhost:8080/StoredScriptTest/Index/{id}"),
+        TestFilename = $"{id}.tests.js",
+      };
+
+      return View(model);
+    }
+
+    IProvidesScript GetScriptProvider(string scriptName)
+    {
+      var providerType = GetScriptProviderType(scriptName);
+      if(providerType == null) return null;
+      return (IProvidesScript) Activator.CreateInstance(providerType);
+    }
+
+    Type GetScriptProviderType(string scriptName)
+    {
+      var providerBaseType = typeof(IProvidesScript);
+      return providerBaseType
+        .Assembly
+        .GetExportedTypes()
+        .FirstOrDefault(x => providerBaseType.IsAssignableFrom(x)
+                             && x.Name.Equals(scriptName, StringComparison.InvariantCultureIgnoreCase));
+    }
+  }
+}
