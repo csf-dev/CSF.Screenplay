0867c64842848a4651845ade192558d7694f27ef
Craig Fowler <craig@csf-dev.com>
2018-03-11 11:55:22 +0000

    WIP #40 - Add the beginnings of the workaround script

---
+++ CSF.Screenplay.Selenium.JavaScriptWorkarounds/ScriptResources/UpdateSelectElementSelection.js
@@ -0,0 +1,121 @@
+﻿function executeScript(argsArray)
+{
+  'use strict';
+  
+  argumentsArrayValidator.validate(argsArray, 2, 3);
+
+  function validateElement(element)
+  {
+    if(element === null || element === undefined)
+      throw new Error('You must provide an HTML element object.');
+    if(!(element instanceof HTMLSelectElement))
+      throw new Error('The element must be an HTML <select> element.');
+  }
+  
+  var optionUpdater = function()
+  {
+    function getOptionByIndex(selectElement, index)
+    {
+      
+    }
+    
+    function getOptionByValue(selectElement, value)
+    {
+      
+    }
+    
+    function getOptionByText(selectElement, text)
+    {
+      
+    }
+    
+    function deselect(optionElement)
+    {
+      
+    }
+    
+    function select(optionElement)
+    {
+      
+    }
+    
+    function selectByIndex(selectElement, index)
+    {
+      var option = getOptionByIndex(selectElement, index);
+      if(!option) return false;
+      select(option);
+      return true;
+    }
+    
+    function selectByValue(selectElement, value)
+    {
+      var option = getOptionByValue(selectElement, index);
+      if(!option) return false;
+      select(option);
+      return true;
+    }
+    
+    function selectByText(selectElement, text)
+    {
+      var option = getOptionByText(selectElement, index);
+      if(!option) return false;
+      select(option);
+      return true;
+    }
+    
+    function deselectByIndex(selectElement, index)
+    {
+      var option = getOptionByIndex(selectElement, index);
+      if(!option) return false;
+      deselect(option);
+      return true;
+    }
+    
+    function deselectByValue(selectElement, value)
+    {
+      var option = getOptionByValue(selectElement, index);
+      if(!option) return false;
+      deselect(option);
+      return true;
+    }
+    
+    function deselectByText(selectElement, text)
+    {
+      var option = getOptionByText(selectElement, index);
+      if(!option) return false;
+      deselect(option);
+      return true;
+    }
+    
+    function deselectAll(selectElement)
+    {
+      var options = selectElement.options;
+      for(var i = 0, len = options.length; i < len; i++)
+        deselect(options.item(i));
+      return options.length > 0;
+    }
+    
+    return {
+      selectByIndex: selectByIndex,
+      selectByValue: selectByValue,
+      selectByText: selectByText,
+      deselectByIndex: deselectByIndex,
+      deselectByValue: deselectByValue,
+      deselectByText: deselectByText,
+      deselectAll: deselectAll,
+    };
+    
+  }();
+  
+  var
+    element = argsArray[0],
+    action = argsArray[1],
+    value = argsArray[2];
+  
+  validateElement(element);
+  
+  if(!optionUpdater.hasOwnProperty(action))
+    throw new Error("The action '" + action + "' was not recognised.");
+  
+  return optionUpdater[action](element, value);
+}
---
+++ CSF.Screenplay.WebTestWebsite/Scripts/script-tests/UpdateSelectElementSelection.tests.js
@@ -0,0 +1,75 @@
+﻿describe('The UpdateSelectElementSelection service', function() {
+  'use strict';
+
+  var scratchArea;
+
+  function clearNodes(element)
+  {
+    while(element.firstChild)
+      element.removeChild(element.firstChild);
+  }
+
+  function addScratchElement(name)
+  {
+    var element = document.createElement(name);
+    scratchArea.appendChild(element);
+    return element;
+  }
+  
+  function addStandardScratchElements()
+  {
+    var divElement = addScratchElement('div');
+    var selectElement = addScratchElement('select');
+    var multiSelectElement = addScratchElement('select');
+    multiSelectElement.setAttribute('multiple', 'multiple');
+    
+    return {
+      div: divElement,
+      select: selectElement,
+      multiSelect: multiSelectElement
+    };
+  }
+
+  beforeEach(function() {
+     scratchArea = document.getElementById('test_scratch_area');
+  });
+
+  afterEach(function() {
+    clearNodes(scratchArea);
+  });
+
+  it('should use the arguments-array validator', function() {
+    spyOn(argumentsArrayValidator, 'validate')
+      .and
+      .returnValue(true);
+    
+    var elements = addStandardScratchElements();
+
+    executeScript([elements.select, 'deselectAll']);
+    expect(argumentsArrayValidator.validate).toHaveBeenCalledWith([elements.select, 'deselectAll'], 2, 3);
+  });
+  
+  describe('should raise an error', function() {
+    it('when the given element is null', function() {
+      expect(function() {
+        executeScript([null, 'deselectAll']);
+      }).toThrowError(Error, /must provide an HTML element/);
+    });
+    
+    it('when the given element is not an HTML select element', function() {
+      var elements = addStandardScratchElements();
+    
+      expect(function() {
+        executeScript([elements.div, 'deselectAll']);
+      }).toThrowError(Error, /must be an HTML <select> element/);
+    });
+    
+    it('when the action is not supported', function() {
+      var elements = addStandardScratchElements();
+    
+      expect(function() {
+        executeScript([elements.select, 'foobar']);
+      }).toThrowError(Error, /'foobar' was not recognised/);
+    });
+  });
+});
