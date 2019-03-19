cf562a6cc2c9ce7d142085a712e77ac2e3599f63
Craig Fowler <craig@csf-dev.com>
2019-03-19 19:33:50 +0000

    Resolve #158 - Remove redundant functionality from Cast API
    This also includes removing some unneeded tests and simplifying
    the object.

--- Tests/CSF.Screenplay.SpecFlow.Tests/Test.feature
+++ Tests/CSF.Screenplay.SpecFlow.Tests/Test.feature
@@ -4,12 +4,12 @@
     I want to keep a running count of numbers
     
 Scenario: Add two numbers
-  Given Joe has the number 50
+  Given Mathias has the number 50
    When he adds 70
    Then he should see the total 120
 
 Scenario: Add three numbers
-  Given Joe has the number 50
+  Given Mathias has the number 50
     And he adds 20
    When he adds 70
    Then he should see the total 140
--- Tests/CSF.Screenplay.Tests/Actors/CastTests.cs
+++ Tests/CSF.Screenplay.Tests/Actors/CastTests.cs
@@ -2,7 +2,6 @@
 using NUnit.Framework;
 using CSF.Screenplay.Actors;
 using Moq;
-using CSF.Screenplay.Scenarios;
 using CSF.FlexDi;
 
 namespace CSF.Screenplay.Tests.Actors
@@ -10,92 +9,7 @@ namespace CSF.Screenplay.Tests.Actors
   [TestFixture,Parallelizable(ParallelScope.All)]
   public class CastTests
   {
-    [Test]
-    public void Add_using_name_sets_actors_name()
-    {
-      // Arrange
-      var cast = GetSut();
-      var name = "joe";
-
-      // Act
-      cast.Add(name);
-      var joe = cast.GetExisting(name);
-
-      // Assert
-      Assert.AreEqual(name, joe.Name);
-    }
-
-    [Test]
-    public void Add_using_guid_sets_scenario_identity()
-    {
-      // Arrange
-      var identity = Guid.NewGuid();
-      var cast = GetSut(identity);
-      var name = "joe";
-
-      // Act
-      cast.Add(name);
-      var joe = cast.GetExisting(name);
-
-      // Assert
-      Assert.AreEqual(identity, joe.ScenarioIdentity);
-    }
-
-    [Test]
-    public void Add_using_name_raises_exception_if_used_twice_with_same_name()
-    {
-      // Arrange
-      var cast = GetSut();
-      var name = "joe";
-
-      // Act
-      cast.Add(name);
-
-      // Act and assert
-      Assert.Throws<DuplicateActorException>(() => cast.Add(name));
-    }
-
-    [Test]
-    public void Add_using_name_does_not_raise_exception_if_used_twice_with_different_names()
-    {
-      // Arrange
-      var cast = GetSut();
-
-      // Act
-      cast.Add("joe");
-      cast.Add("davina");
-
-      // Act and assert
-      Assert.Pass();
-    }
-
-    [Test]
-    public void GetExisting_returns_null_for_nonexistent_actors()
-    {
-      // Arrange
-      var cast = GetSut();
-
-      // Act
-      var joe = cast.GetExisting("joe");
-
-      // Assert
-      Assert.IsNull(joe);
-    }
-
-    [Test]
-    public void GetExisting_returns_instance_for_created_actor()
-    {
-      // Arrange
-      var cast = GetSut();
-      var name = "joe";
-      cast.Add(name);
-
-      // Act
-      var joe = cast.GetExisting(name);
-
-      // Assert
-      Assert.NotNull(joe);
-    }
+    const string Joe = "Joe";
 
     [Test]
     public void Get_creates_new_actor_if_they_do_not_exist()
@@ -104,7 +18,7 @@ namespace CSF.Screenplay.Tests.Actors
       var cast = GetSut();
 
       // Act
-      var joe = cast.Get("joe", (actor) => {});
+      var joe = cast.Get(Joe);
 
       // Assert
       Assert.That(joe, Is.Not.Null);
@@ -118,7 +32,7 @@ namespace CSF.Screenplay.Tests.Actors
       var cast = GetSut(identity);
 
       // Act
-      var joe = cast.Get("joe", (a) => {});
+      var joe = cast.Get(Joe);
 
       // Assert
       Assert.That(joe.ScenarioIdentity, Is.EqualTo(identity));
@@ -131,166 +45,120 @@ namespace CSF.Screenplay.Tests.Actors
       var cast = GetSut();
 
       // Act
-      var joe = cast.Get("joe", (actor) => {});
-      var joeAgain = cast.Get("joe", (actor) => {});
-
-      // Assert
-      Assert.That(joe, Is.SameAs(joeAgain));
-    }
-
-    [Test]
-    public void Get_returns_existing_actor_if_they_already_exist()
-    {
-      // Arrange
-      var cast = GetSut();
-      cast.Add("joe");
-      var joe = cast.GetExisting("joe");
-
-      // Act
-      var joeAgain = cast.Get("joe", (actor) => {});
+      var joe = cast.Get(Joe);
+      var joeAgain = cast.Get(Joe);
 
       // Assert
       Assert.That(joeAgain, Is.SameAs(joe));
     }
 
     [Test]
-    public void Get_applies_scenario_customisation_if_creating_a_new_actor()
+    public void Get_can_get_an_actor_from_a_persona_type()
     {
       // Arrange
       var cast = GetSut();
-      var customisationCallCount = 0;
 
       // Act
-      cast.Get("joe", (a) => customisationCallCount++);
+      var sarah = cast.Get<Sarah>();
 
       // Assert
-      Assert.That(customisationCallCount, Is.EqualTo(1));
+      Assert.That(sarah, Is.Not.Null);
     }
 
     [Test]
-    public void Get_does_not_apply_scenario_customisation_if_actor_already_exists()
+    public void Get_can_get_an_actor_from_a_persona_instance()
     {
       // Arrange
       var cast = GetSut();
-      cast.Add("joe");
-      var customisationCallCount = 0;
+      var persona = new Sarah();
 
       // Act
-      cast.Get("joe", (a) => customisationCallCount++);
+      var sarah = cast.Get(persona);
 
       // Assert
-      Assert.That(customisationCallCount, Is.EqualTo(0));
+      Assert.That(sarah, Is.Not.Null);
     }
 
     [Test]
-    public void Get_applies_scenario_customisation_with_resolver_if_creating_a_new_actor()
-    {
-      // Arrange
-      var resolver = Mock.Of<IResolvesServices>();
-      var cast = GetSut(resolver: resolver);
-
-      // Act
-      cast.Get("joe", (r,a) => r.Resolve<string>());
-
-      // Assert
-      Mock.Get(resolver).Verify(x => x.Resolve<string>(), Times.Once());
-    }
-
-    [Test]
-    public void Get_does_not_apply_scenario_customisation_with_resolver_if_actor_already_exists()
-    {
-      // Arrange
-      var resolver = Mock.Of<IResolvesServices>();
-      var cast = GetSut(resolver: resolver);
-      cast.Add("joe");
-
-      // Act
-      cast.Get("joe", (r,a) => r.Resolve<string>());
-
-      // Assert
-      Mock.Get(resolver).Verify(x => x.Resolve<string>(), Times.Never());
-    }
-
-    [Test]
-    public void GetAll_returns_all_created_actors()
+    public void Get_returns_the_same_actor_if_used_twice_with_the_same_persona_type()
     {
       // Arrange
       var cast = GetSut();
-      cast.Add("joe");
-      cast.Add("davina");
-
-      var joe = cast.GetExisting("joe");
-      var davina = cast.GetExisting("davina");
 
       // Act
-      var all = cast.GetAll();
+      var sarah = cast.Get<Sarah>();
+      var sarahAgain = cast.Get<Sarah>();
 
       // Assert
-      Assert.That(all, Is.EquivalentTo(new [] { joe, davina }));
+      Assert.That(sarahAgain, Is.SameAs(sarah));
     }
 
     [Test]
-    public void Dismiss_removes_all_actors()
+    public void Get_returns_the_same_actor_if_used_twice_with_the_same_persona_instance()
     {
       // Arrange
       var cast = GetSut();
-      cast.Add("joe");
-      cast.Add("davina");
+      var persona = new Sarah();
 
       // Act
-      cast.Dismiss();
+      var sarah = cast.Get(persona);
+      var sarahAgain = cast.Get(persona);
 
       // Assert
-      var all = cast.GetAll();
-      Assert.That(all, Is.Empty);
+      Assert.That(sarahAgain, Is.SameAs(sarah));
     }
 
     [Test]
-    public void Add_adds_the_actor()
+    public void Get_does_not_re_apply_actor_customisation_if_using_the_same_persona_type_twice()
     {
       // Arrange
+      Sarah.StaticGrantCount = 0;
       var cast = GetSut();
-      var name = "joe";
-      var joe = new Actor(name, Guid.NewGuid());
 
       // Act
-      cast.Add(joe);
-      var joeClone = cast.GetExisting(name);
+      var sarah = cast.Get<Sarah>();
+      var sarahAgain = cast.Get<Sarah>();
 
       // Assert
-      Assert.NotNull(joeClone);
-      Assert.AreSame(joe, joeClone);
+      Assert.That(Sarah.StaticGrantCount, Is.EqualTo(1));
     }
 
     [Test]
-    public void Add_raises_exception_if_used_twice_with_same_name()
+    public void Get_does_not_re_apply_actor_customisation_if_using_the_same_persona_instance_twice()
     {
       // Arrange
       var cast = GetSut();
-      var name = "joe";
-      var joe = new Actor(name, Guid.NewGuid());
+      var persona = new Sarah();
 
       // Act
-      cast.Add(joe);
+      var sarah = cast.Get(persona);
+      var sarahAgain = cast.Get(persona);
 
-      // Act and assert
-      Assert.Throws<DuplicateActorException>(() => cast.Add(joe));
-    }
-
-    [Test]
-    public void Add_does_not_raise_exception_if_used_twice_with_different_names()
-    {
-      // Arrange
-      var cast = GetSut();
-      var joe = new Actor("joe", Guid.NewGuid());
-      var davina = new Actor("davina", Guid.NewGuid());
-
-      // Act
-      cast.Add(joe);
-      Assert.That(() => cast.Add(davina), Throws.Nothing);
+      // Assert
+      Assert.That(persona.GrantCount, Is.EqualTo(1));
     }
 
     ICast GetSut(Guid? scenarioGuid = null, IResolvesServices resolver = null)
       => new Cast(scenarioGuid.GetValueOrDefault(Guid.NewGuid()), resolver ?? Mock.Of<IResolvesServices>());
+
+    public class Sarah : Persona
+    {
+      public static int StaticGrantCount { get; set; }
+
+      public int GrantCount { get; private set; }
+
+      public override string Name => "Sarah";
+
+			public override void GrantAbilities(ICanReceiveAbilities actor, IResolvesServices resolver)
+			{
+        GrantCount ++;
+        StaticGrantCount ++;
+			}
+
+      public Sarah()
+      {
+        GrantCount = 0;
+      }
+    }
   }
 }
