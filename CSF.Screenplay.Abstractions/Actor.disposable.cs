4352055cf6f103a0d318ad7945559470da5d6d3f
Craig Fowler <craig@craigfowler.me.uk>
2024-08-30 20:04:15 +0100

    Implement Dispose pattern for Actor

--- CSF.Screenplay.Abstractions/Actor.disposable.cs
+++ CSF.Screenplay.Abstractions/Actor.disposable.cs
@@ -6,20 +6,6 @@ namespace CSF.Screenplay
     {
         bool disposedValue;
 
-        /// <inheritdoc/>
-        public virtual void Dispose()
-        {
-            if (disposedValue) return;
-
-            foreach(var ability in Abilities)
-            {
-                if (ability is IDisposable disposable)
-                    disposable.Dispose();
-            }
-
-            disposedValue = true;
-        }
-
         /// <summary>
         /// Asserts that the current instance is not disposed.
         /// </summary>
@@ -28,6 +14,33 @@ namespace CSF.Screenplay
         {
             if(disposedValue)
                 throw new ObjectDisposedException($"{nameof(Actor)} '{Name}'", "Operations upon a disposed actor are not permitted.");
+        }
+
+        /// <summary>
+        /// Disposes the current instance, via the Dispose Pattern.
+        /// </summary>
+        /// <param name="disposing">A value indicating wherher or not disposal should occur.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue) return;
+
+            if (disposing)
+            {
+                foreach(var ability in Abilities)
+                {
+                    if (ability is IDisposable disposable)
+                        disposable.Dispose();
+                }
+            }
+
+            disposedValue = true;
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
         }
     }
 }
