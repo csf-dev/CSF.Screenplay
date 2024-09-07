dc28345c5f56d555bcf4e962e88f85a3955439c5
Craig Fowler <craig@craigfowler.me.uk>
2024-09-07 10:25:54 +0100

    First attempt at an NUnit 3.6+ integration

---
+++ CSF.Screenplay.NUnit3/ScreenplayAssemblyAttribute.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+
+namespace CSF.Screenplay
+{
+    /// <summary>
+    /// An attribute used to mark an assembly which contains Screenplay-based tests.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This attribute is the core of the NUnit3 <xref href="IntegrationGlossaryItem?text=test+framework+integration"/> with Screenplay.
+    /// In order to run tests with Screenplay, the assembly must be decorated with this attribute.
+    /// </para>
+    /// <para>
+    /// This attribute identifies a concrete implementation of <see cref="IGetsScreenplay"/> which will be used to build and retrieve
+    /// the <see cref="Screenplay"/> instance for running those tests.
+    /// </para>
+    /// </remarks>
+    /// <example>
+    /// <para>
+    /// Decorate your assembly with this attribute using the syntax <c>[assembly: ScreenplayAssembly]</c>.  You may place this
+    /// into any source file, outside of any type declaration.  By convention it would be put into a dedicated source file
+    /// within the <c>Properties</c> project directory.
+    /// </para>
+    /// </example>
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = false)]
+    public class ScreenplayAssemblyAttribute : TestActionAttribute
+    {
+        readonly Lazy<Screenplay> lazyScreenplay;
+
+        /// <summary>
+        /// Gets the <see cref="Screenplay"/> which is to be used for tests contained in the current assembly.
+        /// </summary>
+        /// <returns>The Screenplay.</returns>
+        /// <exception cref="InvalidOperationException">If the Screenplay factory used with the constructor to this
+        /// attribute is invalid or fails to return a non-<see langword="null" /> <see cref="Screenplay"/> instance.</exception>
+        public Screenplay GetScreenplay()
+        {
+            try
+            {
+                return lazyScreenplay.Value;
+            }
+            catch (Exception ex)
+            {
+                throw new InvalidOperationException("An unexpected exception was encountered creating the Screenplay for this test assembly; see the inner exception for details.", ex);
+            }
+        }
+
+        /// <inheritdoc/>
+        public override ActionTargets Targets => ActionTargets.Suite;
+
+        /// <inheritdoc/>
+        public override void AfterTest(ITest test) => GetScreenplay().CompleteScreenplay();
+
+        /// <inheritdoc/>
+        public override void BeforeTest(ITest test) => GetScreenplay().BeginScreenplay();
+
+        /// <summary>
+        /// Initializes a new instance of <see cref="ScreenplayAssemblyAttribute"/>.
+        /// </summary>
+        /// <remarks>
+        /// <para>
+        /// The <paramref name="factoryType"/> specified in this constructor must meet all of the following criteria:
+        /// </para>
+        /// <list type="bullet">
+        /// <item><description>It must be a non-<see langword="null" /> <see cref="Type"/> which derives from <see cref="IGetsScreenplay"/></description></item>
+        /// <item><description>It must have a public parameterless constructor</description></item>
+        /// <item><description>It must return a non-<see langword="null" /> instance of <see cref="Screenplay"/> from its <see cref="IGetsScreenplay.GetScreenplay"/> method</description></item>
+        /// </list>
+        /// </remarks>
+        /// <param name="factoryType">The concrete type of a class which implements <see cref="IGetsScreenplay"/>.</param>
+        public ScreenplayAssemblyAttribute(Type factoryType)
+        {
+            lazyScreenplay = new Lazy<Screenplay>(GetScreenplayFactory(factoryType));
+        }
+
+        static Func<Screenplay> GetScreenplayFactory(Type factoryType)
+        {
+            if (factoryType is null)
+                throw new ArgumentNullException(nameof(factoryType));
+            if (!typeof(IGetsScreenplay).IsAssignableFrom(factoryType))
+                throw new ArgumentException($"The factory type {factoryType.FullName} must derive from {nameof(IGetsScreenplay)}.", nameof(factoryType));
+            if (factoryType.GetConstructor(Type.EmptyTypes) is null)
+                throw new ArgumentException($"The factory type {factoryType.FullName} must have a public parameterless constructor.", nameof(factoryType));
+            
+            return () =>
+            {
+                var factory = InstantiateFactory(factoryType);
+                var screenplay = factory.GetScreenplay();
+                if (screenplay is null)
+                    throw new InvalidOperationException($"The factory type {factoryType.FullName} must return a non-null Screenplay instance from its {nameof(IGetsScreenplay.GetScreenplay)} method.");
+                
+                return screenplay;
+            };
+        }
+
+        static IGetsScreenplay InstantiateFactory(Type factoryType)
+        {
+            try
+            {
+                return (IGetsScreenplay) Activator.CreateInstance(factoryType);
+            }
+            catch(Exception ex)
+            {
+                throw new InvalidOperationException($"The factory type {factoryType.FullName} must not raise an exception from its public parameterless constructor.", ex);
+            }
+        }
+    }
+}
---
+++ CSF.Screenplay.NUnit3/ScreenplayAttribute.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using CSF.Screenplay.Performances;
+using NUnit.Framework;
+using NUnit.Framework.Interfaces;
+using NUnit.Framework.Internal;
+using NUnit.Framework.Internal.Builders;
+using static CSF.Screenplay.ScreenplayLocator;
+
+namespace CSF.Screenplay
+{
+    /// <summary>
+    /// Applied to an assembly, test class or individual test method, indicates that the tests in the corresponding scope are Screenplay tests.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// When a test method, fixture or assembly is decorated with this attribute then all tests with that scope
+    /// (the test method, all test methods in the fixture, or all test methods in the assembly) are to be executed via Screenplay.
+    /// This means that each affected test method will be executed as an <see cref="IPerformance"/>.
+    /// This means that all parameters to the affected test methods will be provided by resolving them from the current
+    /// performance's <see cref="IHasServiceProvider.ServiceProvider"/>.
+    /// See <xref href="DependencyInjectionMainArticle?text=the+article+on+dependency+injection+in+Screenplay"/> for more information
+    /// about what may be injected into test logic from DI, via the test method parameters.
+    /// </para>
+    /// <para>
+    /// Remember that for this attribute to be effective, the <see cref="Assembly"/> which contains the test methods must be decorated with
+    /// <see cref="ScreenplayAssemblyAttribute"/>.  If it is not, then the tests will all fail with exceptions.
+    /// </para>
+    /// </remarks>
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = false)]
+    public class ScreenplayAttribute : Attribute, ITestAction, ITestBuilder
+    {
+        /// <summary>
+        /// The name of an NUnit3 Property for the suite or test description.
+        /// </summary>
+        internal const string DescriptionPropertyName = "Description";
+
+        /// <summary>
+        /// A key for an NUnit3 Property which will hold the current <see cref="IPerformance"/>.
+        /// </summary>
+        internal const string CurrentPerformanceKey = "Current Screenplay Performance";
+
+        /// <summary>
+        /// A key for an NUnit3 Property which will hold the <see cref="IHasPerformanceIdentity.PerformanceIdentity"/> of the current <see cref="IPerformance"/>.
+        /// </summary>
+        internal const string CurrentPerformanceIdentityKey = "Current Screenplay Performance identity";
+
+        /// <summary>
+        /// Gets the targets for the attribute (when performing before/after test actions).
+        /// </summary>
+        /// <value>The targets.</value>
+        public ActionTargets Targets => ActionTargets.Test;
+
+
+        /// <inheritdoc/>
+        public void BeforeTest(ITest test)
+        {
+            var performance = GetPerformance(test);
+            test.Properties.Add(CurrentPerformanceIdentityKey, performance.PerformanceIdentity);
+            performance.BeginPerformance();
+        }
+
+        /// <inheritdoc/>
+        public void AfterTest(ITest test)
+        {
+            var performance = GetPerformance(test);
+            var outcome = GetOutcome();
+            performance.FinishPerformance(outcome);
+        }
+
+        /// <inheritdoc/>
+        public IEnumerable<TestMethod> BuildFrom(IMethodInfo method, Test suite)
+        {
+            if (method is null)
+                throw new ArgumentNullException(nameof(method));
+            if (suite is null)
+                throw new ArgumentNullException(nameof(suite));
+
+            var screenplay = GetScreenplay(method);
+            var performance = CreatePerformance(screenplay, method, suite);
+            var testMethod = GetTestMethod(performance, method, suite);
+            return new[] { testMethod };
+        }
+
+        static IPerformance CreatePerformance(Screenplay screenplay, IMethodInfo method, Test suite)
+        {
+            var namingHierarchy = GetReverseOrderNamingHierarchy(method, suite).ToList();
+            // Reverse it to get it in the correct order
+            namingHierarchy.Reverse();
+            return screenplay.CreatePerformance(namingHierarchy);
+        }
+
+        static IPerformance GetPerformance(ITest test)
+        {
+            return (IPerformance)test.Properties.Get(CurrentPerformanceKey)
+                ?? throw new ArgumentException($"The specified test must contain a property '{CurrentPerformanceKey}' containing an {nameof(IPerformance)}", nameof(test));
+        }
+
+        static TestMethod GetTestMethod(IPerformance performance, IMethodInfo method, Test suite)
+        {
+            var builder = new NUnitTestCaseBuilder();
+            var resolvedTestMethodParameters = (from parameter in method.GetParameters()
+                                                select performance.ServiceProvider.GetService(parameter.ParameterType))
+                .ToArray();
+            var testCaseParameters = new TestCaseParameters(resolvedTestMethodParameters);
+
+            var testMethod = builder.BuildTestMethod(method, suite, testCaseParameters);
+            testMethod.Properties.Add(CurrentPerformanceKey, performance);
+            return testMethod;
+        }
+
+        static IEnumerable<IdentifierAndName> GetReverseOrderNamingHierarchy(IMethodInfo method, ITest suite)
+        {
+            yield return new IdentifierAndName($"{suite.FullName}.{method.Name}", suite.Properties.Get(DescriptionPropertyName)?.ToString());
+
+            for (var currentSuite = suite.Parent;
+                currentSuite != null;
+                currentSuite = currentSuite.Parent)
+            {
+                yield return new IdentifierAndName(currentSuite.FullName, currentSuite.Properties.Get(DescriptionPropertyName)?.ToString());
+            }
+        }
+
+        static bool? GetOutcome()
+        {
+            var result = TestContext.CurrentContext.Result.Outcome.Status;
+            switch (result)
+            {
+                case TestStatus.Passed: return true;
+                case TestStatus.Failed: return false;
+                default: return null;
+            }
+        }
+    }
+}
