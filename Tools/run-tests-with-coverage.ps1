d2d96f474abda8173ac77ec9818617328b198ccf
Craig Fowler <craig@craigfowler.me.uk>
2024-09-07 15:54:36 +0100

    Improve multi-project coverage report

--- Tools/run-tests-with-coverage.ps1
+++ Tools/run-tests-with-coverage.ps1
@@ -4,25 +4,36 @@ $Tfm = "net8.0"
 $Configuration = "Debug"
 Remove-Item $SolutionRoot\TestResults\coverage.json -ErrorAction Ignore
 
-$IsFirst = $true
-
 foreach($project in $TestProjects)
 {
-    if($IsFirst)
+    if($project -eq $TestProjects[0])
     {
-        $IsFirst = $false
+        # First one, no merging results; nothing to merge with!
         coverlet `
             "$SolutionRoot\Tests\$project\bin\$Configuration\$Tfm\$project.dll" `
             --target "dotnet" `
             --targetargs "test $SolutionRoot\Tests\$project --no-build --logger:nunit --test-adapter-path:." `
             -o="$SolutionRoot\TestResults\coverage.json"
     }
+    elseif($project -eq $TestProjects[-1])
+    {
+        # Last one, merge results and output opencover
+        coverlet `
+            "$SolutionRoot\Tests\$project\bin\$Configuration\$Tfm\$project.dll" `
+            --target "dotnet" `
+            --targetargs "test $SolutionRoot\Tests\$project --no-build --logger:nunit --test-adapter-path:." `
+            -f=opencover `
+            -o="$SolutionRoot\TestResults\coverage.opencover.xml"
+    }
     else
     {
+        # Neither first nor last, merge results
         coverlet `
             "$SolutionRoot\Tests\$project\bin\$Configuration\$Tfm\$project.dll" `
             --target "dotnet" `
             --targetargs "test Tests\$project --no-build --logger:nunit --test-adapter-path:." `
-            --merge-with "$SolutionRoot\TestResults\coverage.json"
+            --merge-with "$SolutionRoot\TestResults\coverage.json" `
+            -o="$SolutionRoot\TestResults\coverage.json"
     }
 }
+
