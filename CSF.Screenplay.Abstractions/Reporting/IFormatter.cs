e0db67962487defec9c5dc32e4c53fe90845498b
Craig Fowler <craig@craigfowler.me.uk>
2024-10-01 21:26:13 +0100

    WIP on the rewrite of the reporting system
    This is just interfaces at the moment.

---
+++ CSF.Screenplay.Abstractions/Reporting/IFormatter.cs
@@ -0,0 +1,22 @@
+namespace CSF.Screenplay.Reporting
+{
+    /// <summary>
+    /// An object which can format a value which appears within a Screenplay report.
+    /// </summary>
+    public interface IFormatter
+    {
+        /// <summary>
+        /// Gets a value indicating whether this object is suitable for formatting the specified value.
+        /// </summary>
+        /// <param name="value">The value to be formatted</param>
+        /// <returns><see langword="true" /> if this formatter is suitable for formatting the specified value; <see langword="false" /> if not.</returns>
+        bool CanFormat(object value);
+
+        /// <summary>
+        /// Gets a formatted string which represents the specified value.
+        /// </summary>
+        /// <param name="value">The value to be formatted</param>
+        /// <returns>A formatted string which represents the specified value.</returns>
+        string Format(object value);
+    }
+}
