ef94c7db9c70f8abc2499e6e391da11d198b7da5
Craig Fowler <craig@craigfowler.me.uk>
2024-10-04 13:06:31 +0100

    WIP #208 - Fix tech debt (switch to while loops)

--- CSF.Screenplay/Reporting/ReportFormatCreator.cs
+++ CSF.Screenplay/Reporting/ReportFormatCreator.cs
@@ -31,31 +31,33 @@ namespace CSF.Screenplay.Reporting
         {
             var builder = new StringBuilder(template.Length);
             var results = new List<string>();
-            var maxIndex = template.Length;
+            int maxIndex = template.Length, i = 0;
 
-            for(var i = 0;  i < maxIndex;  i++)
+            while(i < maxIndex)
             {
                 if(template[i] != openBrace)
                 {
                     builder.Append(template[i]);
+                    i++;
                     continue;
                 }
 
                 if(template[i] == openBrace && i < maxIndex - 1 && template[i+1] == openBrace)
                 {
                     builder.Append(openBrace).Append(openBrace);
-                    // We have consumed 2 characters of the template, so we need to double-increment
-                    i++;
+                    // We have consumed 2 characters of the template, hence the need to double-increment
+                    i += 2;
                     continue;
                 }
 
                 if(!TryGetBraceContent(template, i, maxIndex, out var content, out var consumed))
                 {
                     builder.Append(template[i]);
+                    i++;
                     continue;
                 }
 
-                i += consumed;
+                i += consumed + 1;
                 if(results.Contains(content))
                 {
                     builder.Append(openBrace).Append(results.IndexOf(content)).Append(closeBrace);
@@ -74,8 +76,9 @@ namespace CSF.Screenplay.Reporting
         static bool TryGetBraceContent(string template, int startIndex, int maxIndex, out string content, out int charactersConsumed)
         {
             var builder = new StringBuilder(template.Length - startIndex);
+            int i = startIndex + 1, consumed = 1;
 
-            for(int i = startIndex + 1, consumed = 1; i < maxIndex; i++, consumed++)
+            while(i < maxIndex)
             {
                 if(template[i] == closeBrace)
                 {
@@ -83,10 +86,10 @@ namespace CSF.Screenplay.Reporting
                     charactersConsumed = consumed;
                     return true;
                 }
-                else
-                {
-                    builder.Append(template[i]);
-                }
+
+                builder.Append(template[i]);
+                i++;
+                consumed++;
             }
 
             // We never found a matching close-brace!
