db876957d204f597c4f9b2caf3409a816e85e6c6
Craig Fowler <craig@craigfowler.me.uk>
2024-10-09 21:48:59 +0100

    WIP #208 - More test coverage

--- Tests/CSF.Screenplay.Tests/Reporting/PerformanceReportBuilderTests.cs
+++ Tests/CSF.Screenplay.Tests/Reporting/PerformanceReportBuilderTests.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using CSF.Screenplay.Actors;
 using CSF.Screenplay.Performables;
 using CSF.Screenplay.Performances;
@@ -102,6 +103,38 @@ public class PerformanceReportBuilderTest
                                                             && x.PerformancePhase == performancePhase
                                                             && x.PerformableType == "CSF.Screenplay.Performables.StartTheStopwatch"));
     }
+    
+    [Test, AutoMoqData]
+    public void BeginAndEndPerformableShouldBeAbleToCreateAHierarchyOfPerformableReports([Frozen] IFormatsReportFragment formatter,
+                                                                                         PerformanceReportBuilder sut,
+                                                                                         [NamedActor("Joe")] Actor actor,
+                                                                                         bool? outcome,
+                                                                                         TaskPerformable performable1,
+                                                                                         StartTheStopwatch performable2,
+                                                                                         StopTheStopwatch performable3,
+                                                                                         string performancePhase)
+    {
+        Mock.Get(formatter).Setup(x => x.Format(It.IsAny<string>(), actor)).Returns(new ReportFragment("Original", "Formatted", Array.Empty<NameAndValue>()));
+        sut.BeginPerformable(performable1, actor, performancePhase);
+        sut.BeginPerformable(performable2, actor, performancePhase);
+        sut.EndPerformable(performable2, actor);
+        sut.BeginPerformable(performable3, actor, performancePhase);
+        sut.EndPerformable(performable3, actor);
+        sut.EndPerformable(performable1, actor);
+        var report = sut.GetReport(outcome);
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(report.Reportables, Has.Count.EqualTo(1), "Only one reportable should be present at root level");
+            
+            var taskReport = report.Reportables.OfType<PerformableReport>().Single();
+            Assert.That(taskReport.Reportables, Has.Count.EqualTo(2), "Two child reports should be present");
+            Assert.That(taskReport.Reportables.OfType<PerformableReport>().Select(x => x.PerformableType),
+                        Is.EqualTo(new[] { "CSF.Screenplay.Performables.StartTheStopwatch", "CSF.Screenplay.Performables.StopTheStopwatch" }),
+                        "The child reports should be of the correct performable types");
+        });
+    }
+
 
     [Test, AutoMoqData]
     public void BeginAndEndPerformableShouldAddAPerformableReportableWithFallbackReportIfPerformableIsNotReportable(PerformanceReportBuilder sut,
@@ -115,5 +148,85 @@ public class PerformanceReportBuilderTest
         var report = sut.GetReport(outcome);
         Assert.That(report.Reportables,
                     Has.One.Matches<PerformableReport>(x => x.Report == "Joe performed System.String"));
+    }
+
+    [Test, AutoMoqData]
+    public void RecordAssetForCurrentPerformableShouldAddAnAssetToTheReportable(PerformanceReportBuilder sut,
+                                                                                [NamedActor("Joe")] Actor actor,
+                                                                                bool? outcome,
+                                                                                object performable,
+                                                                                string performancePhase,
+                                                                                string assetFilename,
+                                                                                string assetSummary)
+    {
+        sut.BeginPerformable(performable, actor, performancePhase);
+        sut.RecordAssetForCurrentPerformable(assetFilename, assetSummary);
+        sut.EndPerformable(performable, actor);
+        var report = sut.GetReport(outcome);
+
+        Assert.That(report.Reportables.OfType<PerformableReport>().Single().Assets,
+                    Has.One.Matches<PerformableAsset>(x => x.FilePath == assetFilename && x.FileSummary == assetSummary));
+    }
+
+    [Test, AutoMoqData]
+    public void RecordResultForCurrentPerformableShouldAddTheFormattedResultToTheReportable([Frozen] IGetsValueFormatter valueFormatterProvider,
+                                                                                            PerformanceReportBuilder sut,
+                                                                                            [NamedActor("Joe")] Actor actor,
+                                                                                            bool? outcome,
+                                                                                            object performable,
+                                                                                            string performancePhase,
+                                                                                            IValueFormatter valueFormatter,
+                                                                                            object result,
+                                                                                            string resultText)
+    {
+        Mock.Get(valueFormatterProvider).Setup(x => x.GetValueFormatter(It.IsAny<object>())).Returns(valueFormatter);
+        Mock.Get(valueFormatter).Setup(x => x.Format(result)).Returns(resultText);
+        sut.BeginPerformable(performable, actor, performancePhase);
+        sut.RecordResultForCurrentPerformable(result);
+        sut.EndPerformable(performable, actor);
+        var report = sut.GetReport(outcome);
+        Assert.That(report.Reportables,
+                    Has.One.Matches<PerformableReport>(x => x.Result == resultText));
+    }
+
+    [Test, AutoMoqData]
+    public void RecordFailureForCurrentPerformableShouldAddTheExceptionStringToTheReportable(PerformanceReportBuilder sut,
+                                                                                             [NamedActor("Joe")] Actor actor,
+                                                                                             bool? outcome,
+                                                                                             string performable,
+                                                                                             string performancePhase)
+    {
+        sut.BeginPerformable(performable, actor, performancePhase);
+        sut.RecordFailureForCurrentPerformable(new Exception("An error occurred"));
+        var report = sut.GetReport(outcome);
+        Assert.That(report.Reportables,
+                    Has.One.Matches<PerformableReport>(x => x.Exception.Contains("An error occurred") && x.ExceptionIsFromConsumedPerformable == false));
+    }
+
+    [Test, AutoMoqData]
+    public void RecordFailureForCurrentPerformableShouldSetExceptionIsFromConsumedPerformableToTrueIfTheExceptionIsPerformableException(PerformanceReportBuilder sut,
+                                                                                                                                        [NamedActor("Joe")] Actor actor,
+                                                                                                                                        bool? outcome,
+                                                                                                                                        string performable,
+                                                                                                                                        string performancePhase)
+    {
+        sut.BeginPerformable(performable, actor, performancePhase);
+        sut.RecordFailureForCurrentPerformable(new PerformableException("An error occurred"));
+        var report = sut.GetReport(outcome);
+        Assert.That(report.Reportables,
+                    Has.One.Matches<PerformableReport>(x => x.Exception.Contains("An error occurred") && x.ExceptionIsFromConsumedPerformable == true));
+    }
+
+    public class TaskPerformable : IPerformable, ICanReport
+    {
+        public ReportFragment GetReportFragment(IHasName actor, IFormatsReportFragment formatter)
+            => formatter.Format("{Actor} starts then stops their stopwatch after a short pause", actor);
+
+        public async ValueTask PerformAsAsync(ICanPerform actor, CancellationToken cancellationToken = default)
+        {
+            await actor.PerformAsync(new StartTheStopwatch());
+            await Task.Delay(50);
+            await actor.PerformAsync(new StopTheStopwatch());
+        }
     }
 }
