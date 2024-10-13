31479faf9b6b2746b216dc38ce056964ac2fca63
Craig Fowler <craig@craigfowler.me.uk>
2024-10-13 14:35:35 +0100

    WIP #217 - Try to diagnose test problem

--- Tools/appveyor-upload-test-results.ps1
+++ Tools/appveyor-upload-test-results.ps1
@@ -7,7 +7,11 @@ $wc = New-Object 'System.Net.WebClient'
 
 foreach($project in $TestProjects)
 {
+
     $testResultFile = "$SolutionRoot\Tests\$project\TestResults\TestResults.xml"
-    Move-Item $testResultFile "$SolutionRoot\TestResults\$project.TestResults.xml"
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$project.TestResults.xml))
+    $destination = "$SolutionRoot\TestResults\$project.TestResults.xml"
+    Write-Host "Moving from $testResultFile to $destination"
+    Move-Item $testResultFile $destination
+    Write-Host "UploadFile('https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)', (Resolve-Path $destination))"
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $destination))
 }
