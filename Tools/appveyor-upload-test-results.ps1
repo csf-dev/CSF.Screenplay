45901fb6bf57abaf29ef84501cc98f308a930155
Craig Fowler <craig@csf-dev.com>
2024-10-28 13:22:09 +0000

    Update AppVeyor test result upload
    This is done in response to a support ticket: https://help.appveyor.com/discussions/problems/37319-http-500-error-when-uploading-nunit3-test-results-suspected-failure-on-parameterized-test-cases

--- Tools/appveyor-upload-test-results.ps1
+++ Tools/appveyor-upload-test-results.ps1
@@ -5,6 +5,10 @@ $TestProjects = Get-ChildItem $SolutionRoot\Tests\
 
 $wc = New-Object 'System.Net.WebClient'
 
+# Temporary change, made to diagnose test results upload error:
+# https://help.appveyor.com/discussions/problems/37319-http-500-error-when-uploading-nunit3-test-results-suspected-failure-on-parameterized-test-cases
+Push-AppveyorArtifact "$SolutionRoot\Tests\CSF.Screenplay.Tests\TestResults\TestResults.xml"
+
 foreach($project in $TestProjects)
 {
     $testResultFile = "$SolutionRoot\Tests\$project\TestResults\TestResults.xml"
