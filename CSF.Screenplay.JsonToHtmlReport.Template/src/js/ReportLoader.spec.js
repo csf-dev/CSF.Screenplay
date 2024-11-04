e1cf247ee1cc535ce81d1af46ade8e93851089e8
Craig Fowler <craig@craigfowler.me.uk>
2024-11-04 20:59:48 +0000

    WIP #226 - Improve coverage

--- CSF.Screenplay.JsonToHtmlReport.Template/src/js/ReportLoader.spec.js
+++ CSF.Screenplay.JsonToHtmlReport.Template/src/js/ReportLoader.spec.js
@@ -12,3 +12,18 @@ test('ReportLoader should load JSON content from the specified script element', 
 
     expect(jsonData).toEqual({foo: 'bar'});
 });
+
+test('ReportLoader should throw an error if the specified script element is not found', () => {
+    getElementById.mockReturnValue(null);
+
+    const reportLoader = new ReportLoader('scriptId');
+    expect(() => reportLoader.loadJson()).toThrowError('Element with id scriptId not found');
+});
+
+test('ReportLoader should throw an error if the specified script element does not contain valid JSON', () => {
+    const scriptElement = { textContent: 'invalid json' };
+    getElementById.mockReturnValue(scriptElement);
+
+    const reportLoader = new ReportLoader('scriptId');
+    expect(() => reportLoader.loadJson()).toThrowError('Failed to parse JSON content');
+});
