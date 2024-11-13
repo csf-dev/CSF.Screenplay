5416469f0bdb98380772b62eff479a68e431ec5f
Craig Fowler <craig@csf-dev.com>
2024-11-13 13:14:59 +0000

    Resolve #217 - Fix test upload
    This follows the advice given in the AppVeyor support ticket.

--- Tools/appveyor-upload-test-results.ps1
+++ Tools/appveyor-upload-test-results.ps1
@@ -1,17 +1,16 @@
-# Taken from https://www.appveyor.com/docs/running-tests/#uploading-xml-test-results
+# Adapted from https://www.appveyor.com/docs/running-tests/#uploading-xml-test-results
 
 $SolutionRoot = "$PSScriptRoot\.."
 $TestProjects = Get-ChildItem $SolutionRoot\Tests\
 
 $wc = New-Object 'System.Net.WebClient'
 
-# Temporary change, made to diagnose test results upload error:
-# https://help.appveyor.com/discussions/problems/37319-http-500-error-when-uploading-nunit3-test-results-suspected-failure-on-parameterized-test-cases
-Push-AppveyorArtifact "$SolutionRoot\Tests\CSF.Screenplay.Tests\TestResults\TestResults.xml"
-
 foreach($project in $TestProjects)
 {
     $testResultFile = "$SolutionRoot\Tests\$project\TestResults\TestResults.xml"
     Move-Item $testResultFile "$SolutionRoot\TestResults\$project.TestResults.xml"
+    
+    # Intentionally using the 'nunit' endpoint and not 'nunit3'.  See the following ticket for more info:
+    # https://help.appveyor.com/discussions/problems/37319-http-500-error-when-uploading-nunit3-test-results-suspected-failure-on-parameterized-test-cases
     $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$project.TestResults.xml))
 }
