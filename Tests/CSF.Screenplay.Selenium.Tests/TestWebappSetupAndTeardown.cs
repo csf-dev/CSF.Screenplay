e53fb878967473df6f031904045fb8a8ba0f392f
Craig Fowler <craig@craigfowler.me.uk>
2024-12-14 09:41:23 +0000

    WIP #212 - Update build to fix Linux tests
    This follows lessons learned in the CSF.Extensions.WebDriver project.
    * Activate xvfb to provide an X environment for the browser
    * Ensure we're using the right driver version for the env
    * Upgrade to latest beta of CSF.Extensions.WebDriver

--- Tests/CSF.Screenplay.Selenium.Tests/TestWebappSetupAndTeardown.cs
+++ Tests/CSF.Screenplay.Selenium.Tests/TestWebappSetupAndTeardown.cs
@@ -17,22 +17,7 @@ public class TestWebappSetupAndTeardown
     public async Task StartWebAppAsync()
     {
         webAppProcess = Process.Start("dotnet", $"run --project {GetPathToWebappProject()}");
-        using var client = new HttpClient();
-
-        // Wait for the web app to start, up to 30 seconds across 15 attempts
-        for(var attempt = 0; attempt < maxAttempts; attempt++)
-        {
-            try
-            {
-                await client.GetAsync(new Uri(Webster.TestWebappBaseUri, "index.html"));
-                break;
-            }
-            catch(Exception e)
-            {
-                Console.Error.WriteLine($"Failed to connect to web app on attempt {attempt + 1} of {maxAttempts}: " + e.Message);
-                await Task.Delay(TimeSpan.FromSeconds(secondsDelay));
-            }
-        }
+        await WaitForWebAppToBeAvailableAsync();
     }
 
     [OneTimeTearDown]
@@ -40,6 +25,37 @@ public class TestWebappSetupAndTeardown
     {
         webAppProcess?.Kill();
         webAppProcess?.Dispose();
+    }
+
+    /// <summary>
+    /// Waits for the testing web app to be available by attempting to connect to it.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// This method will make a number of attempts to connect to the web app, with a delay between each attempt.
+    /// If the web app is not available after the maximum number of attempts then an exception will be thrown.
+    /// </para>
+    /// </remarks>
+    /// <returns>A task which completes when the web app is available.</returns>
+    async static Task WaitForWebAppToBeAvailableAsync()
+    {
+        using var client = new HttpClient();
+
+        for(var attempt = 0; attempt < maxAttempts; attempt++)
+        {
+            try
+            {
+                await Task.Delay(TimeSpan.FromSeconds(secondsDelay));
+                await client.GetAsync(new Uri(Webster.TestWebappBaseUri, "index.html"));
+                return;
+            }
+            catch(Exception)
+            {
+                // Intentional no-op; we'll just try again
+            }
+        }
+
+        throw new TimeoutException($"The web app was not available after {secondsDelay * maxAttempts} seconds.");
     }
 
     static string GetPathToWebappProject() => Path.Combine("..", "..", "..", "..", "CSF.Screenplay.Selenium.TestWebapp");
