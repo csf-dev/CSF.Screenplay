85a98a4974a7b397c6f35fdde67b607e4195b255
Craig Fowler <craig@craigfowler.me.uk>
2025-12-30 17:29:22 +0000

    Improve tests
    * Avoid splatting a console error in a place that's unwanted
    * Exit with a failure code if any tests fail
    * Exclude the non-test web app from tests

--- Tools/run-tests-with-coverage.ps1
+++ Tools/run-tests-with-coverage.ps1
@@ -1,8 +1,9 @@
 $SolutionRoot = "$PSScriptRoot\.."
-$TestProjects = Get-ChildItem $SolutionRoot\Tests\
+$TestProjects = Get-ChildItem $SolutionRoot\Tests\ -Exclude CSF.Screenplay.Selenium.TestWebapp
 $Tfm = "net8.0"
 $Configuration = "Debug"
 Remove-Item $SolutionRoot\TestResults\* -ErrorAction Ignore
+$PlannedExitCode = 0
 
 foreach($project in $TestProjects)
 {
@@ -12,5 +13,13 @@ foreach($project in $TestProjects)
         --targetargs "test $SolutionRoot\Tests\$project --no-build --logger:nunit --test-adapter-path:." `
         -f=opencover `
         -o="$SolutionRoot\TestResults\$project.opencover.xml"
+
+    if ($LastExitCode -eq 1) {
+        $PlannedExitCode = 1
+    }
+    elseif ($LastExitCode -eq 3) {
+        $PlannedExitCode = 1
+    }
 }
 
+exit $PlannedExitCode
