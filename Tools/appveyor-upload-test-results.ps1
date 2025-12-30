9e81612189e4115a3253d42a9809dab1f86d093d
Craig Fowler <craig@craigfowler.me.uk>
2025-12-30 18:00:41 +0000

    Update the way tests are run

--- Tools/appveyor-upload-test-results.ps1
+++ Tools/appveyor-upload-test-results.ps1
@@ -1,17 +1,14 @@
 # Taken from https://www.appveyor.com/docs/running-tests/#uploading-xml-test-results
 
 $SolutionRoot = "$PSScriptRoot\.."
-$TestProjects = Get-ChildItem $SolutionRoot\Tests\
+$TestProjects = Get-ChildItem $SolutionRoot\Tests\ -Exclude CSF.Screenplay.Selenium.TestWebapp
 
 $wc = New-Object 'System.Net.WebClient'
 
-# Temporary change, made to diagnose test results upload error:
-# https://help.appveyor.com/discussions/problems/37319-http-500-error-when-uploading-nunit3-test-results-suspected-failure-on-parameterized-test-cases
-Push-AppveyorArtifact "$SolutionRoot\Tests\CSF.Screenplay.Tests\TestResults\TestResults.xml"
-
 foreach($project in $TestProjects)
 {
-    $testResultFile = "$SolutionRoot\Tests\$project\TestResults\TestResults.xml"
-    Move-Item $testResultFile "$SolutionRoot\TestResults\$project.TestResults.xml"
-    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$project.TestResults.xml))
+    $projectName = Split-Path $project -Leaf
+    $testResultFile = "$project\TestResults\TestResults.xml"
+    Move-Item $testResultFile "$SolutionRoot\TestResults\$projectName.TestResults.xml"
+    $wc.UploadFile("https://ci.appveyor.com/api/testresults/nunit3/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $SolutionRoot\TestResults\$projectName.TestResults.xml))
 }
--- Tools/run-tests-with-coverage.ps1
+++ Tools/run-tests-with-coverage.ps1
@@ -1,5 +1,5 @@
 $SolutionRoot = "$PSScriptRoot\.."
-$TestProjects = Get-ChildItem $SolutionRoot\Tests\ -Exclude CSF.Screenplay.Selenium.TestWebapp
+$TestProjects = Get-ChildItem -Path $SolutionRoot\Tests\ -Exclude CSF.Screenplay.Selenium.TestWebapp
 $Tfm = "net8.0"
 $Configuration = "Debug"
 Remove-Item $SolutionRoot\TestResults\* -ErrorAction Ignore
@@ -7,12 +7,14 @@ $PlannedExitCode = 0
 
 foreach($project in $TestProjects)
 {
-    coverlet `
-        "$SolutionRoot\Tests\$project\bin\$Configuration\$Tfm\$project.dll" `
+    $projectName = Split-Path $project -Leaf
+    $projectAssembly = "$project\bin\$Configuration\$Tfm\$projectName.dll"
+    Write-Host coverlet `
+        "$projectAssembly" `
         --target "dotnet" `
-        --targetargs "test $SolutionRoot\Tests\$project --no-build --logger:nunit --test-adapter-path:." `
+        --targetargs "test $project --no-build --logger:nunit --test-adapter-path:." `
         -f=opencover `
-        -o="$SolutionRoot\TestResults\$project.opencover.xml"
+        -o="$SolutionRoot\TestResults\$projectName.opencover.xml"
 
     if ($LastExitCode -eq 1) {
         $PlannedExitCode = 1
