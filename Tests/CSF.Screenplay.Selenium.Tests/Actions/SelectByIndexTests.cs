ff8ee683046fa4f8dae03ae0821e955da7a808c6
Craig Fowler <craig@craigfowler.me.uk>
2026-01-03 13:30:05 +0000

    WIP #212 - More test coverage

---
+++ Tests/CSF.Screenplay.Selenium.Tests/Actions/SelectByIndexTests.cs
@@ -0,0 +1,29 @@
+
+using System;
+using CSF.Screenplay.Selenium.Elements;
+using static CSF.Screenplay.PerformanceStarter;
+using static CSF.Screenplay.Selenium.SeleniumPerformableBuilder;
+
+namespace CSF.Screenplay.Selenium.Actions;
+
+[TestFixture]
+public class SelectByIndexTests
+{
+    static readonly ITarget
+        selectElement = new ElementId("selectElement", "the select element"),
+        displayText = new ElementId("display", "the displayable text");
+
+    static readonly NamedUri testPage = new NamedUri("SelectionTests.html", "the test page");
+
+    [Test, Screenplay]
+    public async Task SelectTheOptionFromShouldAddOneSelectedItem(IStage stage)
+    {
+        var webster = stage.Spotlight<Webster>();
+
+        await Given(webster).WasAbleTo(OpenTheUrl(testPage));
+        await When(webster).AttemptsTo(SelectTheOption(0).From(selectElement));
+        var contents = await Then(webster).Should(ReadFromTheElement(displayText).TheText());
+
+        Assert.That(contents, Is.EqualTo($"First{Environment.NewLine}Third"));
+    }
+}
