f7dc7655321753cba57a191fac7f7c3d6dcdf479
Craig Fowler <craig@craigfowler.me.uk>
2026-01-08 20:34:13 +0000

    #212 - Fix two files without namespaces

--- CSF.Screenplay.Selenium/Builders/IBuildsElementPredicates.cs
+++ CSF.Screenplay.Selenium/Builders/IBuildsElementPredicates.cs
@@ -3,21 +3,25 @@ using System;
 using CSF.Screenplay.Selenium.Actions;
 using CSF.Screenplay.Selenium.Elements;
 using CSF.Specifications;
-/// <summary>
-/// An object which may be converted into either a <see cref="WaitUntilPredicate{Boolean}"/> or an <see cref="ISpecificationFunction{SeleniumElement}"/>.
-/// </summary>
-public interface IBuildsElementPredicates
+
+namespace CSF.Screenplay.Selenium.Builders
 {
     /// <summary>
-    /// Gets a <see cref="WaitUntilPredicate{Boolean}"/> which may be used to configure a <see cref="WaitUntilPredicate{Boolean}"/>.
+    /// An object which may be converted into either a <see cref="WaitUntilPredicate{Boolean}"/> or an <see cref="ISpecificationFunction{SeleniumElement}"/>.
     /// </summary>
-    /// <returns>A wait-until predicate object</returns>
-    /// <exception cref="InvalidOperationException">If this instance has not been initialised in a manner compatible with creating Web Driver predicates</exception>
-    WaitUntilPredicate<bool> GetWaitUntilPredicate();
+    public interface IBuildsElementPredicates
+    {
+        /// <summary>
+        /// Gets a <see cref="WaitUntilPredicate{Boolean}"/> which may be used to configure a <see cref="WaitUntilPredicate{Boolean}"/>.
+        /// </summary>
+        /// <returns>A wait-until predicate object</returns>
+        /// <exception cref="InvalidOperationException">If this instance has not been initialised in a manner compatible with creating Web Driver predicates</exception>
+        WaitUntilPredicate<bool> GetWaitUntilPredicate();
 
-    /// <summary>
-    /// Gets a specification function which may be used to filter Selenium elements.
-    /// </summary>
-    /// <returns>A specification function for Selenium elements</returns>
-    ISpecificationFunction<SeleniumElement> GetElementSpecification();
+        /// <summary>
+        /// Gets a specification function which may be used to filter Selenium elements.
+        /// </summary>
+        /// <returns>A specification function for Selenium elements</returns>
+        ISpecificationFunction<SeleniumElement> GetElementSpecification();
+    }
 }
