d67f6ae3311d3522d1ad8a20769908ad8008ab0e
Craig Fowler <craig@craigfowler.me.uk>
2026-01-16 20:58:28 +0000

    Remove unneeded config

--- Tools/Appveyor.before_build.bat
+++
@@ -1,9 +0,0 @@
-@echo on
-
-git submodule update --init --recursive
-
-nuget restore CSF.Screenplay.Selenium.sln
-
-copy /y CSF.Screenplay.Selenium.Tests\App.AppVeyor.config CSF.Screenplay.Selenium.Tests\app.config
-
-@echo off
