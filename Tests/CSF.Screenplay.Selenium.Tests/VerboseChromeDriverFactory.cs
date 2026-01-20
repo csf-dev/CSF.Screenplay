e6e4414bf4c3cb1a4fe5d078d358524072bf6005
Craig Fowler <craig@craigfowler.me.uk>
2026-01-20 21:38:53 +0000

    Back to non-verbose logging
    Also add some docco, also exclude
    duplicate files from docs build.

--- Tests/CSF.Screenplay.Selenium.Tests/VerboseChromeDriverFactory.cs
+++ Tests/CSF.Screenplay.Selenium.Tests/VerboseChromeDriverFactory.cs
@@ -5,6 +5,16 @@ using OpenQA.Selenium.Chrome;
 
 namespace CSF.Screenplay.Selenium;
 
+/// <summary>
+/// An implementation of <see cref="ICreatesWebDriverFromOptions"/> which writes verbose logs to a file.
+/// </summary>
+/// <remarks>
+/// <para>
+/// This driver factory is unused most of the time, but it's helpful if you're trying to diagnose problems with ChromeDriver,
+/// say in a CI build that's been failing.  Select this factory by changing the <c>appsettings.json</c> file and you'll get
+/// log files.
+/// </para>
+/// </remarks>
 public class VerboseChromeDriverFactory : ICreatesWebDriverFromOptions
 {
     public WebDriverAndOptions GetWebDriver(WebDriverCreationOptions options, Action<DriverOptions> supplementaryConfiguration = null)
