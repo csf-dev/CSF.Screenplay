89e642aa7ba3a43b915676899a7c786fea33f102
Craig Fowler <craig@craigfowler.me.uk>
2026-01-27 22:04:45 +0000

    Resolve #250 - Add tests for entering dates

---
+++ CSF.Screenplay.Selenium/Tasks/EnterTheDate.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Globalization;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using CSF.Screenplay.Selenium.Elements;
+using static CSF.Screenplay.Selenium.PerformableBuilder;
+
+namespace CSF.Screenplay.Selenium.Actions
+{
+    /// <summary>
+    /// A <see cref="IPerformable"/> which represents an actor entering a date value into an <c>&lt;input type="date"&gt;</c> element.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// Note that this task is culture-sensitive.  Ensure that the date value is entered into the browser using the culture in which the browser is
+    /// running.
+    /// If no culture information is specified then this task defaults to the current culture: <see cref="CultureInfo.CurrentCulture"/>.
+    /// However, this is not certain to be correct, particularly in remote/cloud configurations where the web browser is operating on different
+    /// infrastructure to the computer which is executing the Screenplay performance.  These two computers might be operating in different cultures.
+    /// </para>
+    /// <para>
+    /// If the date specified to this task is <see langword="null"/> then this task will clear the date from the target.
+    /// </para>
+    /// </remarks>
+    /// <example>
+    /// <para>
+    /// For example, a British English browser <c>en-GB</c> expects dates to be entered in the format ddMMyyyy.
+    /// However, a US English browser <c>en-US</c> expects dates to be entered in the format MMddyyyy.
+    /// </para>
+    /// </example>
+    public class EnterTheDate : IPerformable, ICanReport
+    {
+        const string nonNumericPattern = @"\D";
+        static readonly Regex nonNumeric = new Regex(nonNumericPattern, RegexOptions.Compiled | RegexOptions.IgnoreCase | RegexOptions.CultureInvariant);
+
+        readonly DateTime? date;
+        readonly ITarget target;
+        readonly CultureInfo culture;
+
+        string GetShortDatePattern() => culture.DateTimeFormat.ShortDatePattern;
+
+        string FormatDate() => date.HasValue ? date.Value.ToString(GetShortDatePattern()) : null;
+
+        /// <inheritdoc/>
+        public ValueTask PerformAsAsync(ICanPerform actor, CancellationToken cancellationToken = default)
+        {
+            if(!date.HasValue)
+                return actor.PerformAsync(ClearTheContentsOf(target), cancellationToken);
+
+            var dateText = nonNumeric.Replace(FormatDate(), string.Empty);
+            return actor.PerformAsync(EnterTheText(dateText).Into(target), cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public ReportFragment GetReportFragment(Actor actor, IFormatsReportFragment formatter)
+        {
+            return date.HasValue
+                ? formatter.Format("{Actor} enters the date {Date} into {Target}", actor.Name, FormatDate(), target)
+                : formatter.Format("{Actor} clears the date from {Target}", actor.Name, date, target);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="EnterTheDate"/> class with the specified date.
+        /// </summary>
+        /// <param name="date">The date to enter into the element.</param>
+        /// <param name="target">The element into which to enter the data</param>
+        /// <param name="culture">The culture for which to enter the date</param>
+        public EnterTheDate(DateTime? date, ITarget target, CultureInfo culture = null)
+        {
+            this.date = date;
+            this.target = target ?? throw new ArgumentNullException(nameof(target));
+            this.culture = culture ?? CultureInfo.CurrentCulture;
+        }
+    }
+}
---
+++ Tests/CSF.Screenplay.Selenium.Tests/Tasks/EnterTheDateTests.cs
@@ -0,0 +1,60 @@
+using System;
+using System.Globalization;
+using CSF.Screenplay.Selenium.Elements;
+using static CSF.Screenplay.PerformanceStarter;
+using static CSF.Screenplay.Selenium.PerformableBuilder;
+
+namespace CSF.Screenplay.Selenium.Tasks;
+
+[TestFixture, Parallelizable]
+public class EnterTheDateTests
+{
+    static readonly ITarget
+        inputArea = new ElementId("inputArea", "the input area"),
+        displayText = new ElementId("display", "the displayable text");
+
+    static readonly NamedUri testPage = new NamedUri("InputDateTests.html", "the test page");
+
+    [Test, Screenplay]
+    public async Task EnteringADateShouldYieldTheCorrectValue(IStage stage)
+    {
+        var webster = stage.Spotlight<Webster>();
+
+        await Given(webster).WasAbleTo(OpenTheUrl(testPage));
+        await When(webster).AttemptsTo(EnterTheDate(new DateTime(2025, 11, 12)).Into(inputArea));
+        var result = await Then(webster).Should(ReadFromTheElement(displayText).TheText());
+
+        Assert.That(result, Is.EqualTo("2025-11-12"));
+    }
+    
+    [Test, Screenplay]
+    public async Task EnteringANullDateShouldClearTheValue(IStage stage)
+    {
+        var webster = stage.Spotlight<Webster>();
+
+        await Given(webster).WasAbleTo(OpenTheUrl(testPage));
+        await When(webster).AttemptsTo(EnterTheDate(null).Into(inputArea));
+        var result = await Then(webster).Should(ReadFromTheElement(displayText).TheText());
+
+        Assert.That(result, Is.EqualTo(string.Empty));
+    }
+    
+    [Test, Screenplay]
+    public async Task EnteringADateInAnUnusualCultureShouldYieldIncorrectResults(IStage stage)
+    {
+        var webster = stage.Spotlight<Webster>();
+
+        if(CultureInfo.CurrentCulture.DateTimeFormat.ShortDatePattern.StartsWith("y", StringComparison.InvariantCultureIgnoreCase))
+            Assert.Inconclusive("This test can't be meaningfully run when the current culture uses Y/M/D date formatting");
+
+        await Given(webster).WasAbleTo(OpenTheUrl(testPage));
+        await When(webster).AttemptsTo(EnterTheDate(new DateTime(2025, 11, 12)).Into(inputArea).ForTheCultureNamed("ja-JP"));
+        var result = await Then(webster).Should(ReadFromTheElement(displayText).TheText());
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(result, Is.Not.EqualTo(string.Empty), "The date shouldn't be empty");
+            Assert.That(result, Is.Not.EqualTo("2025-11-12"), "The date shouldn't be the value which was entered either, because of the culture/format difference"); 
+        });
+    }
+}
