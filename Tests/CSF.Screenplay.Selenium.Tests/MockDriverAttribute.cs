ec30f24141922958fc3f5565a745538e928f5d78
Craig Fowler <craig@craigfowler.me.uk>
2026-01-31 22:06:55 +0000

    Resolve #284 - Open URL throws if the Url is not absolute

---
+++ Tests/CSF.Screenplay.Selenium.Tests/MockDriverAttribute.cs
@@ -0,0 +1,23 @@
+using System.Reflection;
+using AutoFixture;
+using CSF.Extensions.WebDriver;
+using CSF.Extensions.WebDriver.Factories;
+using Moq;
+
+namespace CSF.Screenplay.Selenium;
+
+public class MockDriverAttribute : CustomizeAttribute
+{
+    public override ICustomization GetCustomization(ParameterInfo parameter)
+        => new MockDriverCustomization();
+}
+
+public class MockDriverCustomization : ICustomization
+{
+    public void Customize(IFixture fixture)
+    {
+        fixture.Customize<IGetsWebDriver>(c => c.FromFactory((WebDriverAndOptions d) => Mock.Of<IGetsWebDriver>(m => m.GetDefaultWebDriver(null) == d
+                                                                                                                  && m.GetWebDriver(It.IsAny<string>(), null) == d)));
+        fixture.Customize<BrowseTheWeb>(c => c.FromFactory((IGetsWebDriver d) => new BrowseTheWeb(d)));
+    }
+}
