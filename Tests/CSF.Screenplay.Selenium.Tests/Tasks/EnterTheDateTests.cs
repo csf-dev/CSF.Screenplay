191c30009fc11cc5848876d464815a48232edff9
Craig Fowler <craig@craigfowler.me.uk>
2026-02-04 07:44:53 +0000

    Add missing using statement

--- Tests/CSF.Screenplay.Selenium.Tests/Tasks/EnterTheDateTests.cs
+++ Tests/CSF.Screenplay.Selenium.Tests/Tasks/EnterTheDateTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Globalization;
+using System.Linq;
 using CSF.Screenplay.Selenium.Elements;
 using static CSF.Screenplay.PerformanceStarter;
 using static CSF.Screenplay.Selenium.PerformableBuilder;
