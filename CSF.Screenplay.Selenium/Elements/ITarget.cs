144effc95341688caa7abaa4e9f4cb316ae28819
Craig Fowler <craig@craigfowler.me.uk>
2026-02-11 19:58:07 +0000

    WIP #258 - Improve docs for ITarget
    Also add an xrefmap for Selenium, which is a huge coup.
    This will allow me to refer directly to Selenium types in my docs
    and link to their docs!

--- CSF.Screenplay.Selenium/Elements/ITarget.cs
+++ CSF.Screenplay.Selenium/Elements/ITarget.cs
@@ -3,17 +3,29 @@ using OpenQA.Selenium;
 namespace CSF.Screenplay.Selenium.Elements
 {
     /// <summary>
-    /// A target is an object which may be interacted with by Selenium-based Screenplay performables.
+    /// A target is an object which represents one or more HTML elements and which may serve as parameters for performables
+    /// which interact with elements on the web page.
     /// </summary>
     /// <remarks>
     /// <para>
-    /// Targets roughly correspond to the concept of 'elements' in Selenium WebDriver; the targets abstraction may
-    /// by implemented by objects which contain one or more Selenium <c>IWebElement</c> instances.
+    /// See <xref href="SeleniumTargetsAndElementsArticle?text=the+targets+and+elements+article"/> for more information.
+    /// The <c>ITarget</c> interface unifies two Selenium concepts: <see cref="IWebElement"/> instances and the
+    /// <see cref="By"/> class.
+    /// Targets correspond to the concept of HTML elements on a web page and may be used as the parameters for
+    /// <xref href="PerformableGlossaryItem?text=performable+classes"/> which interact with those elements.
     /// </para>
     /// <para>
-    /// Targets might alternatively be implemented by objects which contain selector information, such as a CSS selector, XPath
-    /// an id attribute.  In this scenario the target is conceptually 'a specification for finding elements'.
-    /// A selector/specification-based target may be used to find elements at runtime, via the <see cref="GetElements"/> method.
+    /// The <see cref="SeleniumElement"/> and <see cref="SeleniumElementCollection"/> classes wrap native Selenium
+    /// web element instances. They are concrete references to elements which have been retrieved from the WebDriver.
+    /// In both of these cases, the <see cref="GetElement(IWebDriver)"/> &amp; <see cref="GetElements(IWebDriver)"/>
+    /// methods will often return references to themselves without making any use of the WebDriver parameter.
+    /// </para>
+    /// <para>
+    /// On the other hand, targets which derive from <see cref="Locator"/>, such as <see cref="ElementId"/>, <see cref="ClassName"/>,
+    /// <see cref="CssSelector"/> and <see cref="XPath"/> identify HTML element(s) without being a concrete reference to them.
+    /// A Locator is conceptually 'a specification for finding elements'.
+    /// Locator-based targets will use the WebDriver parameter in their <see cref="GetElement(IWebDriver)"/> and
+    /// <see cref="GetElements(IWebDriver)"/> methods, in order to retrieve concrete element references.
     /// </para>
     /// </remarks>
     public interface ITarget : IHasName
