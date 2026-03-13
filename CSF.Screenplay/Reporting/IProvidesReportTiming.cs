634845e42a1700eafd4e883b2ccabbbb846a7c45
Craig Fowler <craig@craigfowler.me.uk>
2026-03-13 19:03:37 +0000

    Update IProvidesReportTiming.cs
    Put the file in a namespace.

--- CSF.Screenplay/Reporting/IProvidesReportTiming.cs
+++ CSF.Screenplay/Reporting/IProvidesReportTiming.cs
@@ -1,5 +1,8 @@
 using System;
 
+namespace CSF.Screenplay.Reporting
+{
+
 /// <summary>
 /// An object which acts as a stopwatch, intended for used in providing timing data for reports.
 /// </summary>
@@ -18,4 +21,5 @@ public interface IMeasuresTime : IDisposable
     /// <returns>A timespan which is the time elapsed since timing began.</returns>
     /// <exception cref="InvalidOperationException">If this method is used before <see cref="BeginTiming"/> has been executed.</exception>
     TimeSpan GetCurrentTime();
+}
 }
