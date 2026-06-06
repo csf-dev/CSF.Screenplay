90006916c9e23aa1ec057ae6b30d9fa778f675f2
Craig Fowler <craig@craigfowler.me.uk>
2026-06-06 10:19:42 +0100

    WIP #352 - Remove Flow type system
    Since I last tried Flow out 6-8 years ago, it has decidedly lost
    the battle for adoption to TypeScript.  I've found it outdated
    and struggling to keep up with the JS ecosystem since I added
    it to this project.  As such I'm stripping Flow back out and I will
    switch to TS in a forthcoming commit.

--- CSF.Screenplay.JsonToHtmlReport.Template/src/babel.config.js
+++ CSF.Screenplay.JsonToHtmlReport.Template/src/babel.config.js
@@ -1,9 +1,6 @@
 module.exports = {
     presets: [
-        ['@babel/preset-flow', {targets: {node: 'current'}}],
         ['@babel/preset-env', {targets: {node: 'current'}}],
     ],
-    plugins: [
-        ["babel-plugin-syntax-hermes-parser"],
-    ],
+    plugins: [],
 };
